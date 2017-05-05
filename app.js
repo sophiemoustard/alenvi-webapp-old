92ebbfe5ace86355af9492f08970a20fcd6f0692
Pierre HOFMAN <pierre.hofman@epitech.eu>
2017-05-05 19:59:19 +0200

    allow CORS for testing purpose, and modified test battery in index.html

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ const app           = express();
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
+// To comment for PROD, allow to do distant requests (local browser...)
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // Logging errors
 app.use(logger('dev'));
 
