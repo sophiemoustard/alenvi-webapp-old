edaf9593c402128682c07655b76a087b166cd592
Pierre HOFMAN <pierre.hofman@epitech.eu>
2017-05-13 00:54:58 +0200

    update login test and passpor-facebook to find user and update / create automatically

--- config/passport.js
+++ config/passport.js
@@ -12,44 +12,49 @@ passport.use(new FacebookStrategy(facebookConfig, function(accessToken, refreshT
   console.log("refreshToken", refreshToken);
   console.log("profile", profile);
   process.nextTick(function() {
-    User.findOne({'facebook.facebookId': profile.id}, function(err, user) {
-      if (err) {
+    var payload = {
+      "facebook.facebookId": profile.id,
+      "facebook.email": profile.emails[0].value,
+      "facebook.access_token": accessToken
+    };
+    User.findOneAndUpdate({'facebook.facebookId': profile.id}, payload, {'upsert': true}, function(err, user) {
+      if(err) {
         return done(err);
+      } else {
+        return done(null, user);
       }
-      if (!user) {
-        var payload = {
-          "facebook.facebookId": profile.id,
-          "facebook.email": profile.emails[0].value,
-          "facebook.access_token": accessToken
-        };
-        var newPayload = _.pickBy(payload);
-        var newUser = User(
-          newPayload
-        );
-        newUser.save(function(err, user) {
-          if (err) {
-            console.error(err);
-            if (err)
-              return response.error(res, 500, translate[language].unexpectedBehavior);
-          }
-          // user.
-          // return response.success(res, translate[language].userSaved, user);
-          //user = {};
-          done(err, user);
-        });
-      }
-      else {
-        done(err, user);
-      }
-    })
+    });
+    // User.findOne({'facebook.facebookId': profile.id}, function(err, user) {
+    //   if (err) {
+    //     return done(err);
+    //   }
+    //   if (!user) {
+    //     var payload = {
+    //       "facebook.facebookId": profile.id,
+    //       "facebook.email": profile.emails[0].value,
+    //       "facebook.access_token": accessToken
+    //     };
+    //     var newPayload = _.pickBy(payload);
+    //     var newUser = User(
+    //       newPayload
+    //     );
+    //     newUser.save(function(err, user) {
+    //       if (err) {
+    //         console.error(err);
+    //         if (err)
+    //           return response.error(res, 500, translate[language].unexpectedBehavior);
+    //       }
+    //       // user.
+    //       // return response.success(res, translate[language].userSaved, user);
+    //       //user = {};
+    //       done(err, user);
+    //     });
+    //   }
+    //   else {
+    //     done(err, user);
+    //   }
+    // })
   })
-
-  // OrCreate({'facebook.facebookId': profile.id}, function(err, user) {
-  //   if (err) {
-  //     return done(err);
-  //   }
-  //   return done(null, profile);
-  // })
 }));
 
 passport.serializeUser(function(user, done) {
