d816529831b2b9dc3f5ff945a66a1fc591034eeb
Pierre HOFMAN <pierre.hofman@epitech.eu>
2017-06-26 13:55:21 +0200

    fix router export error

--- routes/api/bot.js
+++ routes/api/bot.js
@@ -6,3 +6,5 @@ const botController   = require('../../controllers/botController');
 router.get('/authorize', botController.authorize);
 router.get('/:_id', botController.getUserData); // not protected because we don't want user to re-enter password everytime from the bot
 router.get('/facebook/account_linking', botController.facebookAccountLinking);
+
+module.exports = router;
