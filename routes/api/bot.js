410ed68dab5a63c09337fc820d16908134111dba
Pierre HOFMAN <pierre.hofman@epitech.eu>
2017-06-26 17:00:01 +0200

    fix request API name

--- routes/api/bot.js
+++ routes/api/bot.js
@@ -5,6 +5,6 @@ const botController   = require('../../controllers/botController');
 
 router.get('/authorize', botController.authorize);
 router.get('/facebook/account_linking', botController.facebookAccountLinking);
-router.get('/:_id', botController.getUserByParamId); // not protected because we don't want user to re-enter password everytime from the bot
+router.get('/user/:_id', botController.getUserByParamId); // not protected because we don't want user to re-enter password everytime from the bot
 
 module.exports = router;
