01a89eb55dc05cdfe38ea6da0f253c2b7bffe7ee
Pierre HOFMAN <pierre.hofman@epitech.eu>
2017-07-25 11:57:00 +0200

    VERY quick fix to test routes / actualites alenvi

--- routes/api/ogust.js
+++ routes/api/ogust.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const customerController = require('./../../controllers/Ogust/customerController');
+// const customerController = require('./../../controllers/Ogust/customerController');
 
-router.get('/customers/:id', customerController.getCustomerByCustomerId);
+// router.get('/customers/:id', customerController.getCustomerByCustomerId);
 // router.get('/customers/:id/info', customerController.getThirdPartyInformationsByCustomerId);
 // router.patch('customers/:id/info', customerController.editThirdPartyInformationsByCustomerId);
 
