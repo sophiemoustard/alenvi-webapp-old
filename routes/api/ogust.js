1f767a4ecf266495e2cdf4c19238eed9b1e07a69
Jean-Christophe TREBALAG <jean-christophetrebalag@MacBook-Pro-de-Jean-Christophe.local>
2017-07-31 15:34:30 +0200

    Add GET token test route

--- routes/api/ogust.js
+++ routes/api/ogust.js
@@ -1,13 +1,26 @@
 const express = require('express');
+
 const router = express.Router();
 
 const { checkOgustToken } = require('../../helpers/checkOgustToken');
+const tokenConfig = require('../../config/strategies').token;
+const tokenProcess = require('../../helpers/tokenProcess');
 
 const customerController = require('./../../controllers/Ogust/customerController');
 const employeeController = require('./../../controllers/Ogust/employeeController');
 const tokenController = require('./../../controllers/Ogust/tokenController');
 
-router.get('/token', tokenController.get);
+router.get('/token', tokenProcess.decode({ secret: tokenConfig.secret }), tokenController.get);
+if (process.env.NODE_ENV == 'development') {
+	router.get('/tests/token/:id', (req, res, next) => {
+		const jwt = require('jsonwebtoken');
+		const payload = {
+			_id: req.params.id
+		};
+		const token = jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: '24h' });
+		res.status(200).json({ success: true, message: 'Token bien encrypté !', data: { token } });
+	});
+}
 
 // Routes protection by token
 router.use(checkOgustToken);
