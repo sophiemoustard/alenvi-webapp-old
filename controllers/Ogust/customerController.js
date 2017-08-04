b5eaaa356c514100161ddd6a108d16b77c0e468d
Jean-Christophe TREBALAG <jean-christophetrebalag@MacBook-Pro-de-Jean-Christophe.local>
2017-08-04 13:24:35 +0200

    Fix script name changed

--- controllers/Ogust/customerController.js
+++ controllers/Ogust/customerController.js
@@ -103,7 +103,6 @@ const editThirdPartyInformation = async (req, res) => {
       return res.status(400).json({ success: false, message: translate[language].missingParameters });
     }
     const thirdPartyInfos = await customers.editThirdPartyInformationByCustomerId(req.headers['x-ogust-token'], req.params.id, req.query.third_party || 'C', req.body.arrayValues);
-    console.log(thirdPartyInfos.body);
     if (thirdPartyInfos.body.status == 'KO') {
       res.status(400).json({ success: false, message: thirdPartyInfos.body.message });
     } else {
