62df84dd1f15f79ebe1117711071522541a0cc21
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2017-10-24 17:20:32 +0200

    Fix 404 error condition in calendar component

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -58,7 +58,7 @@ export default {
         }
       } catch (e) {
         console.error(e)
-        if (e.status === 404 && this.events.length !== 0) {
+        if (e.status === 404) {
           Toast.create("Aucune intervention dans la période demandée");
         } else {
           Toast.create("Erreur de chargement des données :/ Si le problème persiste, contacte l'équipe technique :)")
