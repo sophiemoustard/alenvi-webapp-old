1fb4dfa18d73ca15f1a9b4bc45abbe7b77e78e35
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2017-11-08 14:46:55 +0100

    Remove some log #2

--- src/components/models/Users.js
+++ src/components/models/Users.js
@@ -1,8 +1,5 @@
 export default {
   async getAllsectors (context, token) {
-    console.log(process.env);
-    console.log(process.env.NODE_ENV);
-    console.log(process.env.API_HOSTNAME);
     const sectorsRaw = await context.$http.get(`${process.env.API_HOSTNAME}/users/sectors`, { headers: { 'x-access-token': token } });
     return sectorsRaw.data.data.sectors;
   }
