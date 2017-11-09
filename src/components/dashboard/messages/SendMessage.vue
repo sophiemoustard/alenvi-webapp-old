421b43fd1f1bbf4df512ef2dfccad58dc983938d
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2017-11-09 14:38:00 +0100

    Refactoring sendMessage component

--- src/components/dashboard/messages/SendMessage.vue
+++ src/components/dashboard/messages/SendMessage.vue
@@ -5,7 +5,7 @@
       <q-select v-model="selectedSector" :options="orderedSectors" separator />
     </q-field>
     <q-field icon="message">
-      <q-input v-model="message.content" placeholder="Ecrire message..." type="textarea" :min-rows="2" :after="[sendIcon]"/>
+      <q-input v-model="message.content" placeholder="Ecrire message..." type="textarea" :min-rows="2" :after="[{ icon: 'send', content: true, handler: handleMessage }]"/>
     </q-field>
     </div>
   </div>
@@ -53,42 +53,12 @@ export default {
   computed: {
     orderedSectors () {
       return _.sortBy(this.sectors, ['value']);
-    },
-    sendIcon () {
-      // const vm = this; no need of temp variables as using arrow function
-      return {
-        icon: 'send',
-        content: true,
-        handler: async () => {
-          await this.storeMessage();
-          const sectorUserList = this.sectorUserList[this.selectedSector];
-          this.message.success = 0;
-          this.message.failed = 0;
-          Loading.show({ message: 'Envoi messages...' });
-          for (let i = 0, l = sectorUserList.length; i < l; i++) {
-            // console.log(selectedSector[i]._id);
-            const sentMessage = await this.sendMessage(this.message.id, sectorUserList[i]._id);
-            const recipient = {
-              success: sentMessage,
-              recipientId: sectorUserList[i]._id
-            };
-            await this.addMessageRecipient(this.message.id, recipient);
-          }
-          Loading.hide();
-          Dialog.create({
-            title: 'Récapitulatif',
-            message: `Envoyés: ${this.message.success} / Echec: ${this.message.failed}`,
-            buttons: ['Fermer']
-          });
-          // Toast.create('Message(s) envoyé(s)');
-        }
-      }
     }
   },
   methods: {
     async getSectors () {
       try {
-        this.sectorUserList = await users.getAllsectors(this, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1OTQ3ZDFhZWZmNmMyN2NlMDc0MDU2NWEiLCJpYXQiOjE1MDk5NTg1MzEsImV4cCI6MTUxMDA0NDkzMX0.rYa1pYL44WmSlYZJm_J91vRinPAQhjW00eK6Zs2MTpI');
+        this.sectorUserList = await users.getAllsectors(this, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1OTQ3ZDFhZWZmNmMyN2NlMDc0MDU2NWEiLCJpYXQiOjE1MTAyMjMwMzMsImV4cCI6MTUxMDMwOTQzM30.W0ypw9laACPprKjWrlJi9mAvcw7sqqpv3KgBGR4zm9I');
         for (const k in this.sectorUserList) {
           this.sectors.push({
             label: this.correspSectors[k],
@@ -105,7 +75,7 @@ export default {
           message: this.message.content,
           sectors: this.selectedSector
         };
-        const storedMessage = await messages.storeMessage(this, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1OTQ3ZDFhZWZmNmMyN2NlMDc0MDU2NWEiLCJpYXQiOjE1MDk5NTg1MzEsImV4cCI6MTUxMDA0NDkzMX0.rYa1pYL44WmSlYZJm_J91vRinPAQhjW00eK6Zs2MTpI', data, '59ca1e938cc5c5001251ea1e');
+        const storedMessage = await messages.storeMessage(this, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1OTQ3ZDFhZWZmNmMyN2NlMDc0MDU2NWEiLCJpYXQiOjE1MTAyMjMwMzMsImV4cCI6MTUxMDMwOTQzM30.W0ypw9laACPprKjWrlJi9mAvcw7sqqpv3KgBGR4zm9I', data, '59ca1e938cc5c5001251ea1e');
         this.message.id = storedMessage.data.data.message._id;
       } catch (e) {
         console.error(e);
@@ -113,7 +83,7 @@ export default {
     },
     async sendMessage (messageId, recipientId) {
       try {
-        await messages.sendMessage(this, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1OTQ3ZDFhZWZmNmMyN2NlMDc0MDU2NWEiLCJpYXQiOjE1MDk5NTg1MzEsImV4cCI6MTUxMDA0NDkzMX0.rYa1pYL44WmSlYZJm_J91vRinPAQhjW00eK6Zs2MTpI', messageId, recipientId);
+        await messages.sendMessage(this, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1OTQ3ZDFhZWZmNmMyN2NlMDc0MDU2NWEiLCJpYXQiOjE1MTAyMjMwMzMsImV4cCI6MTUxMDMwOTQzM30.W0ypw9laACPprKjWrlJi9mAvcw7sqqpv3KgBGR4zm9I', messageId, recipientId);
         this.message.success++;
         return true;
       } catch (e) {
@@ -124,10 +94,32 @@ export default {
     },
     async addMessageRecipient (messageId, data) {
       try {
-        await messages.addMessageRecipientById(this, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1OTQ3ZDFhZWZmNmMyN2NlMDc0MDU2NWEiLCJpYXQiOjE1MDk5NTg1MzEsImV4cCI6MTUxMDA0NDkzMX0.rYa1pYL44WmSlYZJm_J91vRinPAQhjW00eK6Zs2MTpI', messageId, data);
+        await messages.addMessageRecipientById(this, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1OTQ3ZDFhZWZmNmMyN2NlMDc0MDU2NWEiLCJpYXQiOjE1MTAyMjMwMzMsImV4cCI6MTUxMDMwOTQzM30.W0ypw9laACPprKjWrlJi9mAvcw7sqqpv3KgBGR4zm9I', messageId, data);
       } catch (e) {
         console.error(e);
       }
+    },
+    async handleMessage () {
+      await this.storeMessage();
+      const sectorUserList = this.sectorUserList[this.selectedSector];
+      this.message.success = 0;
+      this.message.failed = 0;
+      Loading.show({ message: 'Envoi messages...' });
+      for (let i = 0, l = sectorUserList.length; i < l; i++) {
+        // console.log(selectedSector[i]._id);
+        const sentMessage = await this.sendMessage(this.message.id, sectorUserList[i]._id);
+        const recipient = {
+          success: sentMessage,
+          recipientId: sectorUserList[i]._id
+        };
+        await this.addMessageRecipient(this.message.id, recipient);
+      }
+      Loading.hide();
+      Dialog.create({
+        title: 'Récapitulatif',
+        message: `Envoyés: ${this.message.success} / Echec: ${this.message.failed}`,
+        buttons: ['Fermer']
+      });
     }
   }
 }
