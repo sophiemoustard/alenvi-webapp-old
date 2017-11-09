c6bd4920802a314e91b2f658ec99a912ef62b5df
Pierre HOFMAN <pierre.hofman@epitech.eu>
2017-11-09 14:46:00 +0100

    Add testing fields on future account welcome

--- src/components/dashboard/users/PigiAccounts.vue
+++ src/components/dashboard/users/PigiAccounts.vue
@@ -1,13 +1,40 @@
 <template>
-  <div>
-    PIGI accounts
+  <div class="layout-padding row justify-center">
+    <div style="width: 700px; max-width: 90vw;">
+      <p class="caption">Création de compte</p>
+      <q-field icon="phone" helper="Numéro sans délimiteur (espaces, points...)">
+        <q-input v-model="text" float-label="Numéro de téléphone" :after="[{ icon: '', content: true, handler: handleMessage }]"/>
+      </q-field>
+      <p class="caption">Envoi de code pour compte existant</p>
+      <q-field icon="phone" helper="Numéro sans délimiteur (espaces, points...)">
+        <q-input v-model="text" float-label="Numéro de téléphone" :after="[sendIcon]"/>
+      </q-field>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-}
+  import {
+    QField,
+    QInput,
+    QBtn
+  } from 'quasar';
+
+  export default {
+    components: {
+      QField,
+      QInput,
+      QBtn
+    },
+    data() {
+      return {
+        text: ''
+      }
+    }
+  }
+
 </script>
 
 <style lang="stylus">
+
 </style>
