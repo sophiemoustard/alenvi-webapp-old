01f8cad36d8e146ce4c3f6abb5eedacac08b298b
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2017-11-10 20:35:13 +0100

    Add main axios instance

---
+++ src/helpers/axiosInstances/main.js
@@ -0,0 +1,32 @@
+import Vue from 'Vue'
+import axios from 'axios'
+import { Cookies } from 'quasar'
+
+import alenvi from './../token/alenvi'
+import ogustToken from './../token/getOgustToken'
+import redirect from './../redirect'
+
+const instance = axios.create();
+
+instance.interceptors.request.use(async function (config) {
+  if (!config.url.match(/ogust/i) && (config.url.match(/users$/i) || config.url.match(/users\/authenticate/i) || config.url.match(/calendar$/i))) {
+    return config;
+  }
+  if (!Cookies.get('alenvi_token')) {
+    const refresh = await alenvi.refreshAlenviCookies(Vue);
+    if (!refresh) {
+      redirect.redirectToLogin(Vue);
+      return config;
+    }
+  }
+  axios.defaults.headers.common['x-access-token'] = Cookies.get('alenvi_token');
+  if (config.url.match(/ogust/i)) {
+    const token = await ogustToken.getOgustToken(Vue);
+    config.headers.common['x-ogust-token'] = token;
+  }
+  return config;
+}, function (err) {
+  return Promise.reject(err);
+});
+
+export const alenviAxios = instance;
