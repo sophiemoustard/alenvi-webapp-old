e2fcc1d808edd577969fd130578b31ef7a6296f6
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2017-11-13 15:05:26 +0100

    Add modifications planning to navTabs

--- src/components/dashboard/users/PigiAccounts.vue
+++ src/components/dashboard/users/PigiAccounts.vue
@@ -3,11 +3,11 @@
     <div style="width: 700px; max-width: 90vw;">
       <p class="caption">Création de compte Alenvi</p>
       <q-field icon="phone" helper="Numéro sans délimiteur (espaces, points...)">
-        <q-input v-model="text" float-label="Numéro de téléphone" :after="[{ icon: 'send', content: true, handler: handleMessage }]"/>
+        <!-- <q-input v-model="text" float-label="Numéro de téléphone" :after="[{ icon: 'send', content: true, handler: handleMessage }]"/> -->
       </q-field>
       <p class="caption">Envoi de code pour compte existant</p>
       <q-field icon="phone" helper="Numéro sans délimiteur (espaces, points...)">
-        <q-input v-model="text" float-label="Numéro de téléphone" :after="[{ icon: 'send', content: true, handler: handleMessage }]"/>
+        <!-- <q-input v-model="text" float-label="Numéro de téléphone" :after="[{ icon: 'send', content: true, handler: handleMessage }]"/> -->
       </q-field>
       <br>
       <q-search v-model="searchUserFromMobilePhone" :debounce="600" placeholder="Numéro auxiliaire" type="tel" stack-label="Numéro de téléphone" />
