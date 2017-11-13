8fa06307f24dc4ce73ab9609d1002d42d0ca99ca
Pierre HOFMAN <pierre.hofman@epitech.eu>
2017-11-13 15:21:28 +0100

    Add some basic tests on phone number retrieving on pigi account

--- src/components/dashboard/users/PigiAccounts.vue
+++ src/components/dashboard/users/PigiAccounts.vue
@@ -3,14 +3,15 @@
     <div style="width: 700px; max-width: 90vw;">
       <p class="caption">Création de compte Alenvi</p>
       <q-field icon="phone" helper="Numéro sans délimiteur (espaces, points...)">
-        <q-input v-model="text" float-label="Numéro de téléphone" :after="[{ icon: 'send', content: true, handler: handleMessage }]"/>
+        <q-input v-model="phoneNbr" float-label="Numéro de téléphone" :after="[{ icon: 'send', content: true, handler: handlePhone }]"/>
       </q-field>
       <p class="caption">Envoi de code pour compte existant</p>
       <q-field icon="phone" helper="Numéro sans délimiteur (espaces, points...)">
-        <q-input v-model="text" float-label="Numéro de téléphone" :after="[{ icon: 'send', content: true, handler: handleMessage }]"/>
+        <q-input v-model="phoneNbr" float-label="Numéro de téléphone" :after="[{ icon: 'send', content: true, handler: handlePhone }]"/>
       </q-field>
-      <br>
-      <q-search v-model="searchUserFromMobilePhone" :debounce="600" placeholder="Numéro auxiliaire" type="tel" stack-label="Numéro de téléphone" />
+      <p v-if="employee">Est-ce bien l'auxiliaire {{ employee.first_name }} {{ employee.last_name }} que tu souhaites accueillir ?</p>
+      <!-- <br>
+      <q-search v-model="searchUserFromMobilePhone" :debounce="600" placeholder="Numéro auxiliaire" type="tel" stack-label="Numéro de téléphone" /> -->
     </div>
   </div>
 </template>
@@ -19,24 +20,38 @@
   import {
     QField,
     QInput,
-    QSearch
+    QSearch,
+    Dialog
   } from 'quasar';
+
+  import users from '../../models/Users'
+  import ogust from '../../models/Ogust';
 
   export default {
     components: {
       QField,
       QInput,
-      QSearch
+      QSearch,
+      Dialog
     },
     data() {
       return {
-        text: '',
-        searchUserFromMobilePhone: ''
+        phoneNbr: '',
+        // searchUserFromMobilePhone: ''
+        employee: ''
       }
     },
     methods: {
-      handleMessage() {
-        return ;
+      async handlePhone() {
+        try {
+          const payload = {
+            mobile_phone: this.phoneNbr
+          }
+          const res = await ogust.getEmployees(payload);
+          this.employee = res[0];
+        } catch (e) {
+          console.error(e);
+        }
       }
     }
   }
