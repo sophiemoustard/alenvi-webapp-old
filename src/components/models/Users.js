9a2f28c3bd6aa3aa11a93abf38b4da5cf4896cf2
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2017-11-16 15:24:29 +0100

    Users showAll can now be used with both instances of axios

--- src/components/models/Users.js
+++ src/components/models/Users.js
@@ -1,12 +1,18 @@
 import { alenviAxios } from '../../helpers/alenviAxios'
+import axios from 'axios'
 
 export default {
-  async showAll (params) {
+  async showAll (params, token = null) {
     try {
-      const employeeIdRaw = await alenviAxios.get(`${process.env.API_HOSTNAME}/users`, { params });
+      let employeeIdRaw;
+      if (token) {
+        employeeIdRaw = await axios.get(`${process.env.API_HOSTNAME}/users`, { params, headers: { 'x-access-token': token } });
+      } else {
+        employeeIdRaw = await alenviAxios.get(`${process.env.API_HOSTNAME}/users`, { params });
+      }
       return employeeIdRaw.data.data.users;
     } catch (e) {
-      console.error(e);
+      console.error(e.response);
     }
   }
 }
