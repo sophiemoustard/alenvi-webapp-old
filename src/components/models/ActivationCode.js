3bb4bb1298259f28a499ebf3af3f637f4b524d05
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2017-11-27 17:42:30 +0100

    Fixing responses data to allow token retrieving

--- src/components/models/ActivationCode.js
+++ src/components/models/ActivationCode.js
@@ -4,10 +4,10 @@ import axios from 'axios'
 export default {
   async create (params) {
     const codeRaw = await alenviAxios.post(`${process.env.API_HOSTNAME}/activation`, params);
-    return codeRaw.data.data.activationCode;
+    return codeRaw.data.data;
   },
   async check (params) {
     const activationCodeRaw = await axios.post(`${process.env.API_HOSTNAME}/activation/check`, params);
-    return activationCodeRaw.data.data.activationCode;
+    return activationCodeRaw.data.data;
   }
 }
