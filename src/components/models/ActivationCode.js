3ee293b15b88c8db126bec73cb7fe0ea0e3f711c
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2017-11-29 10:39:18 +0100

    Updating some request due to changes in API

--- src/components/models/ActivationCode.js
+++ src/components/models/ActivationCode.js
@@ -4,10 +4,10 @@ import axios from 'axios'
 export default {
   async create (params) {
     const codeRaw = await alenviAxios.post(`${process.env.API_HOSTNAME}/activation`, params);
-    return codeRaw.data.data;
+    return codeRaw.data.data.activationData;
   },
-  async check (params) {
-    const activationCodeRaw = await axios.post(`${process.env.API_HOSTNAME}/activation/check`, params);
+  async check (code) {
+    const activationCodeRaw = await axios.get(`${process.env.API_HOSTNAME}/activation/${code}`);
     return activationCodeRaw.data.data;
   }
 }
