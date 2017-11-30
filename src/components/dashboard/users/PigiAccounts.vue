68441aa97114865962e40634aa9b6a9e5417bbd3
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2017-11-30 11:38:57 +0100

    Remove ogust check as user will not be in ogust when sending SMS

--- src/components/dashboard/users/PigiAccounts.vue
+++ src/components/dashboard/users/PigiAccounts.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="layout-padding row justify-center">
     <div style="width: 700px; max-width: 90vw;">
-      <p class="caption">Création de compte Alenvi</p>
+      <p class="caption">Création de compte Alenvi et Ogust</p>
+      <select-sector v-model="selectedSector"></select-sector>
+      <q-field icon="phone" helper="Numéro sans délimiteur (espaces, points...)">
+        <q-input :disable="!selectedSector" v-model="phoneNbr" float-label="Numéro de téléphone" :after="[{ icon: 'send', content: true, handler: handlePhone }]"/>
+      </q-field>
+      <!-- <p class="caption">Envoi de code pour compte existant</p>
       <q-field icon="phone" helper="Numéro sans délimiteur (espaces, points...)">
         <q-input v-model="phoneNbr" float-label="Numéro de téléphone" :after="[{ icon: 'send', content: true, handler: handlePhone }]"/>
-      </q-field>
-      <p class="caption">Envoi de code pour compte existant</p>
-      <q-field icon="phone" helper="Numéro sans délimiteur (espaces, points...)">
-        <q-input v-model="phoneNbr" float-label="Numéro de téléphone" :after="[{ icon: 'send', content: true, handler: handlePhone }]"/>
-      </q-field>
+      </q-field> -->
       <!-- <br>
       <q-search v-model="searchUserFromMobilePhone" :debounce="600" placeholder="Numéro auxiliaire" type="tel" stack-label="Numéro de téléphone" /> -->
     </div>
@@ -16,79 +17,79 @@
 </template>
 
 <script>
+import { QField, QInput, QSearch, Dialog, Toast } from 'quasar';
 
-  import twilio from '../../../helpers/twilio'
+import users from '../../models/Users';
+import ogust from '../../models/Ogust';
+import twilio from '../../../helpers/twilio';
+import activationCode from '../../models/ActivationCode';
+import SelectSector from '../../SelectSector';
 
-  import {
+export default {
+  components: {
     QField,
     QInput,
     QSearch,
-    Dialog,
-    Toast
-  } from 'quasar';
-
-  import users from '../../models/Users'
-  import ogust from '../../models/Ogust'
-  import activationCode from '../../models/ActivationCode'
-
-  export default {
-    components: {
-      QField,
-      QInput,
-      QSearch,
-      Toast
-    },
-    data() {
-      return {
-        phoneNbr: '',
-        // searchUserFromMobilePhone: ''
-        employee: ''
-      }
-    },
-    methods: {
-      async handlePhone() {
+    Toast,
+    SelectSector
+  },
+  data() {
+    return {
+      phoneNbr: '',
+      // searchUserFromMobilePhone: ''
+      employee: '',
+      selectedSector: ''
+    };
+  },
+  methods: {
+    async handlePhone() {
+      try {
+        // const payload = {
+        //   mobile_phone: this.phoneNbr
+        // };
+        // const res = await ogust.getEmployees(payload);
+        // this.employee = res[0];
         try {
-          const payload = {
-            mobile_phone: this.phoneNbr
-          }
-          const res = await ogust.getEmployees(payload);
-          this.employee = res[0];
-          Dialog.create({
-            title: 'Accueil auxiliaire',
-            message: `Accueillir ${this.employee.first_name} ${this.employee.last_name} ?`,
-            buttons: [
-              {
-                label: 'Non',
-                handler: () => {
-                  Toast.create('Envoi annulé.');
-                }
-              },
-              {
-                label: 'Oui',
-                raised: true,
-                color: 'positive',
-                handler: async () => {
-                  try {
-                    const { code } = await activationCode.create({ mobile_phone: this.employee.mobile_phone });
-                    const message = await twilio.sendSMS(this.employee.mobile_phone, { activationCode: code });
-                    console.log('SMS envoyé =', message);
-                    console.log('Auxiliaire accueilli !');
-                  } catch (error) {
-                    Toast.create(`Erreur lors de l'envoi du SMS`);
-                  }
-                }
-              }
-            ]
-          })
-        } catch (e) {
-          console.error(e);
+          const { code } = await activationCode.create({
+            mobile_phone: this.phoneNbr,
+            sector: this.selectedSector
+          });
+          const message = await twilio.sendSMS(this.phoneNbr, {
+            activationCode: code
+          });
+          console.log('SMS envoyé =', message);
+          console.log('Auxiliaire accueilli !');
+        } catch (error) {
+          Toast.create(`Erreur lors de l'envoi du SMS`);
         }
+        // Dialog.create({
+        //   title: 'Accueil auxiliaire',
+        //   message: `Accueillir ${this.employee.first_name} ${this.employee.last_name} ?`,
+        //   buttons: [
+        //     {
+        //       label: 'Non',
+        //       handler: () => {
+        //         Toast.create('Envoi annulé.');
+        //       }
+        //     },
+        //     {
+        //       label: 'Oui',
+        //       raised: true,
+        //       color: 'positive',
+        //       handler: async () => {
+
+        //       }
+        //     }
+        //   ]
+        // })
+      } catch (e) {
+        console.error(e);
       }
     }
   }
-
+};
 </script>
 
 <style lang="stylus" scoped>
-  @import '~variables'
+@import '~variables';
 </style>
