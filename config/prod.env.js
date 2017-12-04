c88c6dbf5d29bfa44f5383e2a9d172a06776ec17
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2017-12-04 10:40:46 +0100

    Add some env variables

--- config/prod.env.js
+++ config/prod.env.js
@@ -14,6 +14,9 @@ if (module_exists('../.env')) {
     OGUST_PRIVATE_KEY: JSON.stringify(process.env.OGUST_PRIVATE_KEY),
     OGUST_PUBLIC_KEY: JSON.stringify(process.env.OGUST_PUBLIC_KEY),
     TOKEN_SECRET: JSON.stringify(process.env.TOKEN_SECRET),
+    CLOUDINARY_API_KEY: JSON.stringify(process.env.CLOUDINARY_API_KEY),
+    CLOUDINARY_API_SECRET: JSON.stringify(process.env.CLOUDINARY_API_SECRET),
+    CLOUDINARY_CLOUD_NAME: JSON.stringify(process.env.CLOUDINARY_CLOUD_NAME),
     MESSENGER_LINK: JSON.stringify(process.env.MESSENGER_LINK)
   }
 }
