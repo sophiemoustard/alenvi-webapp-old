ec57d25a0164cf9f21790bd8eba7280a08919e02
Pierre HOFMAN <pierre.hofman@epitech.eu>
2017-12-18 15:00:36 +0100

    Update future users list

---
+++ src/components/dashboard/users/Users.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="layout-padding row justify-center">
+    <div style="width: 700px; max-width: 90vw;">
+      <p class="caption">Blabla</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { QCard } from 'quasar';
+
+import users from '../../models/Users';
+import ogust from '../../models/Ogust';
+
+export default {
+  components: {
+    QCard,
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~variables';
+</style>
