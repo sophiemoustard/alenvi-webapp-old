806f03e1762a67144b230a00f50ddc010c0e3c4f
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2017-12-21 13:47:25 +0100

    Add redirection to dashboard

--- src/helpers/redirect.js
+++ src/helpers/redirect.js
@@ -3,5 +3,8 @@ import router from '../router'
 export default {
   redirectToLogin () {
     router.push('/dashboard/login');
+  },
+  redirectToDashboard () {
+    router.push('/dashboard');
   }
 }
