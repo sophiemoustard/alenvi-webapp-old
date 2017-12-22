112c81279edc38c951ce848608d30cf87993ca48
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2017-12-22 14:26:19 +0100

    Test redirect to https

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const history = require('connect-history-api-fallback');
 
 app.use(history());
 
+app.use((req, res, next) => {
+  if (req.secure) {
+    return res.redirect(['https://', req.hostname, req.url].join(''));
+  }
+  next();
+})
+
 app.use(express.static(__dirname + '/dist'));
 
 app.set('port', (process.env.PORT || 8080));
