0ed7903b3e0f1c2be949b0aa1cc70d11750e7f43
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-01-10 13:52:55 +0100

    Add ajax bar for the entire app

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,20 @@
   <!-- Don't drop "q-app" class -->
   <div id="q-app">
     <router-view />
+    <q-ajax-bar color="primary" />
   </div>
 </template>
 
 <script>
+import { QAjaxBar } from 'quasar'
 /*
  * Root component
  */
-export default {}
+export default {
+  components: {
+    QAjaxBar
+  }
+}
 </script>
 
 <style lang="stylus">
