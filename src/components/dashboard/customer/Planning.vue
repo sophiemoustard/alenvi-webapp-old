3bc191b7a28a9a267d7ceeb63e48f2bbf9dd3406
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-01-12 11:45:24 +0100

    Rename calendar customer component to planning

--- src/components/dashboard/customer/Planning.vue
+++ src/components/dashboard/customer/Planning.vue
@@ -1,31 +1,69 @@
 <template>
-  <div class="layout-padding">
-    <div class="row justify-center">
-      <div style="width: 700px; max-width: 90vw;">
-        <p>Planning :)</p>
-      </div>
-    </div>
+  <div class="container">
+    <scheduler :events="events" @getData="getEventsData"></scheduler>
   </div>
 </template>
 
 <script>
-// import {  } from 'quasar';
+import { Toast } from 'quasar'
+import moment from 'moment'
+import Scheduler from '../../scheduler/Scheduler.vue'
+import Ogust from '../../models/Ogust'
+import 'dhtmlx-scheduler'
 
-import users from '../../models/Users';
-import ogust from '../../models/Ogust';
+moment.locale('fr');
 
 export default {
   components: {
+    Scheduler
   },
   data () {
     return {
+      events: [],
+      title: '',
+      personId: ''
+    }
+  },
+  async mounted () {
+    this.getEventsData();
+  },
+  watch: {
+    events: function (value) {
+      scheduler.parse(this.events, 'json');
     }
   },
   methods: {
+    async getEventsData () {
+      try {
+        this.personId = 345735114; // ID customer
+        // this.title = personData.title;
+        this.events = await Ogust.getOgustEvents(null, '/calendar/events', this.personId, 'customer');
+      } catch (e) {
+        console.error(e)
+        if (e.status === 404) {
+          Toast.create('Aucune intervention dans la période demandée');
+        } else {
+          Toast.create("Erreur de chargement des données :/ Si le problème persiste, contactez l'équipe technique :)")
+        }
+      }
+    }
   }
-};
+}
 </script>
 
-<style lang="stylus" scoped>
-@import '~variables';
+<style>
+  .alenvi_event div {
+        background-color: #E2007A !important;
+        color: white !important;
+    }
+
+    .dhx_cal_event_line.alenvi_event {
+        background-color: #E2007A !important;
+        color: white !important;
+    }
+
+    .dhx_cal_event_clear.alenvi_event {
+        background-color: #E2007A !important;
+        color: white !important;
+    }
 </style>
