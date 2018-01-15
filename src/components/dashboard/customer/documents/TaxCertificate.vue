586d188ed52750ffbc91a64a7ea8e1812fd347fe
Pierre HOFMAN <pierre.hofman@epitech.eu>
2018-01-15 14:28:52 +0100

    Add tax certificate fix when there are tax cerficates but no download link

--- src/components/dashboard/customer/documents/TaxCertificate.vue
+++ src/components/dashboard/customer/documents/TaxCertificate.vue
@@ -3,17 +3,18 @@
     <div class="row justify-center">
       <!-- <div style="width: 700px; max-width: 90vw;"> -->
         <p v-if="Object.keys(fiscalAttests).length === 0">Aucune attestation fiscale disponible</p>
-        <q-card inline v-for="(fiscalAttest, index) in fiscalAttests" :key="index" v-if="fiscalAttest.print_url" style="cursor: pointer">
+        <q-card inline v-for="(fiscalAttest, index) in fiscalAttests" :key="index" style="cursor: pointer">
           <q-card-title class="text-center">
             {{fiscalAttest.period_start}}
           </q-card-title>
-          <q-card-actions align="around">
+          <q-card-actions align="around" v-if="fiscalAttest.print_url">
             <q-btn flat round small color="primary">
               <a :href="fiscalAttest.print_url" download>
                 <q-icon name="file download" />
               </a>
             </q-btn>
           </q-card-actions>
+          <q-card-main v-if="!fiscalAttest.print_url">Bientôt disponible !</q-card-main>
         </q-card>
       <!-- </div> -->
     </div>
@@ -26,13 +27,14 @@ import ogust from '../../../models/Ogust';
 
 import moment from 'moment';
 
-import { QCard, QCardTitle, QCardActions, QBtn, QIcon } from 'quasar';
+import { QCard, QCardTitle, QCardActions, QBtn, QIcon, QCardMain } from 'quasar';
 
 export default {
   components: {
     QCard,
     QCardTitle,
     QCardActions,
+    QCardMain,
     QBtn,
     QIcon
   },
