603cfdfb74f133c8d1d5118ff51ee00807831e4b
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-01-26 16:36:27 +0100

    Search enabled + add checkbox if customer helper is already in DB (thus email was sent)

--- src/components/dashboard/users/AddHelper.vue
+++ src/components/dashboard/users/AddHelper.vue
@@ -8,18 +8,18 @@
           <q-input v-model="email" float-label="Email" :after="[{ icon: 'send', content: true, handler: handleEmail }]"/>
         </q-field> -->
         <!-- TEST by typing the beginning of a client's name -->
-        <!-- <q-search v-model="terms" placeholder="Commencez à entrer le nom d'un bénéficiaire...">
+        <q-search v-model="terms" placeholder="Commencez à entrer le nom d'un bénéficiaire...">
           <q-autocomplete @search="search" @selected="selected" />
-        </q-search> -->
+        </q-search>
         <!-- Find client's helpers on the fly -->
-        <!-- <q-item tag="label" v-for="(helper, index) in helpers" :key="index">
+        <q-item v-if="checked !== null" tag="label" v-for="(helper, index) in helpers" :key="index">
           <q-item-side>
-            <q-checkbox v-model="checked"></q-checkbox>
+            <q-checkbox v-model="helper.checked" :disable="helper.checked" @change="handleEmail(helper)"></q-checkbox>
           </q-item-side>
           <q-item-main>
             <q-item-tile label>{{ helper.first_name }} {{ helper.last_name }}</q-item-tile>
           </q-item-main>
-        </q-item> -->
+        </q-item>
         
         <p>Un mot de passe sera généré automatiquement.</p>
       </div>
@@ -51,82 +51,84 @@ export default {
   },
   data () {
     return {
-      checked: true,
+      checked: null,
       email: '',
       terms: '',
-      helpers: '',
-      config: {
-        title: 'Liste des aidants Ogust',
-        noHeader: false,
-        bodyStyle: {
-          maxHeight: '700px'
-        },
-        rowHeight: 'auto',
-        pagination: {
-          rowsPerPage: 10,
-          options: [10, 20, 30]
-        },
-        messages: {
-          noData: 'Pas d\'aidant ou de bénéficiaire'
-        },
-        labels: {
-          allCols: 'Colonnes (toutes)',
-          rows: 'Lignes',
-          search: 'Rechercher'
-        },
-        responsive: true
-      },
-      columns: [
-        {
-          label: 'Prénom aidant',
-          field: 'first_name',
-          width: '75px',
-          filter: false,
-          sort: true,
-          type: 'string',
-        },
-        {
-          label: 'Nom aidant',
-          field: 'last_name',
-          width: '75px',
-          filter: false,
-          sort: true,
-          type: 'string',
-        },
-        {
-          label: 'Nom bénéficiaire',
-          field: 'lastNameClient', // TODO: add all last name from clients in this object
-          width: '75px',
-          filter: false,
-          sort: true,
-          type: 'string',
-        },
-        {
-          label: 'Enregistré ?',
-          field: 'isRegistered',
-          filter: false,
-          sort: true,
-          type: 'boolean',
-          width: '75px'
-        }
-      ]
+      helpers: ''
+      // config: {
+      //   title: 'Liste des aidants Ogust',
+      //   noHeader: false,
+      //   bodyStyle: {
+      //     maxHeight: '700px'
+      //   },
+      //   rowHeight: 'auto',
+      //   pagination: {
+      //     rowsPerPage: 10,
+      //     options: [10, 20, 30]
+      //   },
+      //   messages: {
+      //     noData: 'Pas d\'aidant ou de bénéficiaire'
+      //   },
+      //   labels: {
+      //     allCols: 'Colonnes (toutes)',
+      //     rows: 'Lignes',
+      //     search: 'Rechercher'
+      //   },
+      //   responsive: true
+      // },
+      // columns: [
+      //   {
+      //     label: 'Prénom aidant',
+      //     field: 'first_name',
+      //     width: '75px',
+      //     filter: false,
+      //     sort: true,
+      //     type: 'string',
+      //   },
+      //   {
+      //     label: 'Nom aidant',
+      //     field: 'last_name',
+      //     width: '75px',
+      //     filter: false,
+      //     sort: true,
+      //     type: 'string',
+      //   },
+      //   {
+      //     label: 'Nom bénéficiaire',
+      //     field: 'lastNameClient', // TODO: add all last name from clients in this object
+      //     width: '75px',
+      //     filter: false,
+      //     sort: true,
+      //     type: 'string',
+      //   },
+      //   {
+      //     label: 'Enregistré ?',
+      //     field: 'isRegistered',
+      //     filter: false,
+      //     sort: true,
+      //     type: 'boolean',
+      //     width: '75px'
+      //   }
+      // ]
     }
   },
   methods: {
-    async handleEmail () {
+    async handleEmail (helper) {
       try {
-        const params = { email: this.email };
-        const customer = await ogust.getCustomers(params);
-        if (!customer[0]) {
-          return Toast.create('Impossible de trouver cet utilisateur')
-        }
+        // const params = { email };
+        // const customer = await ogust.getCustomers(params);
+        // if (!customer[0]) {
+        //   return Toast.create('Impossible de trouver cet utilisateur')
+        // }
+        console.log(helper.checked);
         Dialog.create({
           title: 'Envoi email',
-          message: `Voulez-vous accueillir l'aidant ${customer[0].last_name} ?`,
+          message: `Voulez-vous accueillir l'aidant ${helper.last_name} ?`,
           buttons: [
             {
               label: 'Non',
               handler () {
+                helper.checked = false;
                 console.log('Disagreed...')
               }
             },
@@ -135,30 +137,30 @@ export default {
               handler: async () => {
                 try {
                   // Check if user already exist in Alenvi DB: if yes, send email, if no create customer then send email
-                  const user = await users.showAll({ customer_id: customer[0].id_customer });
+                  // const user = await users.showAll({ customer_id: customer[0].id_customer });
                   const password = randomize('0', 6);
-                  if (!user) {
-                    const userCreated = await users.create({
+                  if (helper.checked) {
+                    await users.create({
                       local: {
-                        email: this.email,
+                        email: helper.email,
                         password
                       },
-                      customer_id: customer[0].id_customer,
-                      lastname: customer[0].last_name,
+                      customer_id: helper.id_customer,
+                      lastname: helper.last_name,
                       role: 'Client'
                     });
-                    Toast.create(`Utilisateur ${customer[0].last_name} créé dans la base Alenvi`);
-                  } else {
-                    // const user = await users.update({ customer_id: customer[0].id_customer });
-                  }
-                  const sendEmail = await email.sendWelcome({
+                    Toast.create(`Utilisateur ${helper.last_name} créé dans la base Alenvi`);
+                  } // else
+                  // const user = await users.update({ customer_id: customer[0].id_customer });
+                  //
+                  await email.sendWelcome({
                     sender: {
-                      email: 'contact@alenvi.io'
+                      email: 'test@alenvi.io'
                     },
                     receiver: {
-                      email: this.email,
-                      title: customer[0].title,
-                      name: customer[0].last_name,
+                      email: helper.email,
+                      title: helper.title,
+                      name: helper.last_name,
                       password
                     }
                   });
@@ -178,7 +180,7 @@ export default {
     async search (terms, done) {
       try {
         let customers = await ogust.getCustomers({ lastName: `${terms}%` });
-        if (Object.keys(customers).length == 0) {
+        if (Object.keys(customers).length === 0) {
           return done([]);
         }
         // Object of object to array of object
@@ -196,7 +198,11 @@ export default {
     async selected (item) {
       try {
         this.helpers = await ogust.getCustomerContacts(item.id_customer);
-        console.log(this.helpers);
+        for (const k in this.helpers) {
+          const user = await users.showAll({ 'local.email': this.helpers[k].email });
+          this.checked = user.length !== 0;
+          this.$set(this.helpers[k], 'checked', this.checked);
+        }
       } catch (e) {
         console.error(e);
       }
