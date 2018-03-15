680a6236ea87f3b58613b93524f58a66b97ae959
Pierre HOFMAN <pierre.hofman@epitech.eu>
2018-03-15 11:02:51 +0100

    Fix certificate check upload

--- src/pages/signup/SignUpComplete.vue
+++ src/pages/signup/SignUpComplete.vue
@@ -90,8 +90,8 @@
                 <q-uploader name="certificates" :url="docsUploadUrl" :headers="headers"
                 :additional-fields="[{ name: 'fileName', value: `certificate_${user.firstname}_${user.lastname}` }]"
                 float-label="Diplômes et / ou certicats" @finish="afterUpload()" multiple auto-expand extensions=".jpg, .jpeg, .png, .pdf"/>
-                <p class="upload-done" v-if="alenviUser && alenviUser.administrative.certificates">Fichier(s) mis en ligne <q-icon name="check" /></p>
-                <p class="upload-not-done" v-if="alenviUser && !alenviUser.administrative.certificates">Fichier manquant <q-icon name="warning" /></p>
+                <p class="upload-done" v-if="alenviUser && alenviUser.administrative.certificates[0]">Fichier(s) mis en ligne <q-icon name="check" /></p>
+                <p class="upload-not-done" v-if="alenviUser && !alenviUser.administrative.certificates[0]">Fichier manquant <q-icon name="warning" /></p>
               </q-field>
               <q-field icon="mdi-account-card-details" :error="$v.user.administrative.mutualFund.$error" error-label="Champ requis">
                 <q-uploader name="mutualFund" :url="docsUploadUrl" :headers="headers"
