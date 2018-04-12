2de987e358b3c78068aad0fd943cfc11f729a96a
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-04-12 15:10:51 +0200

    Add global css for body and titles + add modal when clicking auxiliary row in IntegrationTracking.vue

--- src/pages/dashboard/auxiliaries/IntegrationTracking.vue
+++ src/pages/dashboard/auxiliaries/IntegrationTracking.vue
@@ -1,4 +1,5 @@
 <template>
+  <q-page>
     <q-table
       :data="filteredUserList"
       :columns="columns"
@@ -25,7 +26,7 @@
           label="Historique intégration"
           class="col-3" />
       </template>
-      <q-tr slot="body" slot-scope="props" class="cursor-pointer" @click.native="goToDriveFolder(props.row)">
+      <q-tr slot="body" slot-scope="props" class="cursor-pointer" @click.native="getAuxiliaryInfo(props.row)">
         <q-td v-for="col in props.cols" :key="col.name" :props="props">
           <span v-if="col.name === 'firstSmsDate' || col.name === 'auxiliary'">{{ col.value }}</span>
           <div v-else>
@@ -35,6 +36,34 @@
         </q-td>
       </q-tr>
     </q-table>
+    <q-modal v-if="auxiliary" v-model="isOpened" :content-css="{ maxWidth: '80vw', maxHeight: '90vh', padding: '20px' }">
+      <div class="row justify-end">
+        <q-btn icon="clear" color="primary" flat @click="isOpened = false" />
+      </div>
+      <h5 class="q-my-md">Détail {{ auxiliary.name }}</h5>
+      <div class="row q-mb-md">
+        <q-btn icon="folder" label="Dossier Google Drive" color="primary" flat @click="goTo(auxiliary.driveFolderLink)" />
+      </div>
+      <div class="row q-mb-md gutter-sm">
+        <div v-for="(info, index) in auxiliary.info" :key="index" inline class="col-xs-12 col-md-4 row justify-center">
+          <q-card style="width: 100%">
+            <q-card-title>{{ info.title }}</q-card-title>
+            <q-card-main>
+              <q-list>
+                <q-item v-for="(data, index) in info.data" :key="index">
+                  <q-item-main>{{ data.label }}</q-item-main>
+                  <q-item-side>
+                    <q-icon name="done" color="positive" v-if="data.done" />
+                    <q-icon name="clear" color="negative" v-if="!data.done" />
+                  </q-item-side>
+                </q-item>
+              </q-list>
+            </q-card-main>
+          </q-card>
+        </div>
+      </div>
+    </q-modal>
+  </q-page>
 </template>
 
 <script>
@@ -44,6 +73,8 @@ export default {
   data () {
     return {
       userList: [],
+      auxiliary: null,
+      isOpened: false,
       isChecked: false,
       isComplete: false,
       filter: '',
@@ -98,21 +129,21 @@ export default {
         {
           name: 'personalInfo',
           label: 'Informations personnelles',
-          field: 'personalInfo',
+          field: row => row.personalInfo.done,
           align: 'center',
           sortable: true
         },
         {
           name: 'paymentInfo',
           label: 'Informations de paiement',
-          field: 'paymentInfo',
+          field: row => row.paymentInfo.done,
           align: 'center',
           sortable: true
         },
         {
           name: 'miscDocuments',
           label: 'Documents annexes',
-          field: 'miscDocuments',
+          field: row => row.miscDocuments.done,
           align: 'center',
           sortable: true
         }
@@ -156,24 +187,75 @@ export default {
                 pigiConnection: userList[i].facebook
                   ? !!userList[i].facebook.address
                   : '-',
-                personalInfo:
-                  !!ogustUserList[k].date_of_birth &&
+                personalInfo: {
+                  data: {
+                    dateOfBirth: {
+                      label: 'Date de naissance',
+                      done: !!ogustUserList[k].date_of_birth
+                    },
+                    placeOfBirth: {
+                      label: 'Lieu de naissance',
+                      done: !!ogustUserList[k].place_of_birth
+                    },
+                    stateOfBirth: {
+                      label: 'Département de naissance',
+                      done: !!ogustUserList[k].state_of_birth
+                    },
+                    countryOfBirth: {
+                      label: 'Pays de naissance',
+                      done: !!ogustUserList[k].country_of_birth
+                    },
+                  },
+                  done: !!ogustUserList[k].date_of_birth &&
                   !!ogustUserList[k].place_of_birth &&
                   !!ogustUserList[k].state_of_birth &&
-                  !!ogustUserList[k].country_of_birth,
-                paymentInfo:
-                  userList[i].administrative &&
+                  !!ogustUserList[k].country_of_birth
+                },
+                paymentInfo: {
+                  data: {
+                    iban: {
+                      label: 'IBAN',
+                      done: userList[i].administrative &&
+                  userList[i].administrative.payment &&
+                  userList[i].administrative.payment.rib
+                        ? !!userList[i].administrative.payment.rib.iban : false
+                    },
+                    bic: {
+                      label: 'BIC',
+                      done: userList[i].administrative &&
+                  userList[i].administrative.payment &&
+                  userList[i].administrative.payment.rib
+                        ? !!userList[i].administrative.payment.rib.bic : false
+                    },
+                  },
+                  done: userList[i].administrative &&
                   userList[i].administrative.payment &&
                   userList[i].administrative.payment.rib
                     ? !!userList[i].administrative.payment.rib.iban &&
-                      !!userList[i].administrative.payment.rib.iban
-                    : '-',
-                miscDocuments: userList[i].administrative
-                  ? !!userList[i].administrative.idCard &&
-                    !!userList[i].administrative.healthAttest &&
-                    !!userList[i].administrative.certificates &&
-                    !!userList[i].administrative.phoneInvoice
-                  : '-',
+                      !!userList[i].administrative.payment.rib.bic
+                    : '-'
+                },
+                miscDocuments: {
+                  data: {
+                    idCard: {
+                      label: 'CNI',
+                      done: userList[i].administrative ? !!userList[i].administrative.idCard : false
+                    },
+                    healthAttest: {
+                      label: 'Attestation carte vitale',
+                      done: userList[i].administrative ? !!userList[i].administrative.healthAttest : false
+                    },
+                    certificates: {
+                      label: 'Diplômes et/ou certificats',
+                      done: userList[i].administrative ? userList[i].administrative.certificates.length !== 0 : false
+                    }
+                  },
+                  done: userList[i].administrative
+                    ? !!userList[i].administrative.idCard &&
+                  !!userList[i].administrative.healthAttest &&
+                  userList[i].administrative.certificates.length !== 0
+                    : '-'
+                },
                 complete: userList[i].administrative.signup.complete,
                 driveFolderLink: userList[i].administrative.driveFolder ? userList[i].administrative.driveFolder.link : false
               });
@@ -187,10 +269,30 @@ export default {
     paginationLabel (start, end, total) {
       return `${start} - ${end} de ${total}`;
     },
-    goToDriveFolder (row) {
-      if (row.driveFolderLink) {
-        openURL(row.driveFolderLink);
-      }
+    getAuxiliaryInfo (row) {
+      this.auxiliary = {
+        info: {
+          personalInfo: {
+            title: 'Informations personnelles',
+            data: row.personalInfo.data
+          },
+          paymentInfo: {
+            title: 'Informations de paiement',
+            data: row.paymentInfo.data
+          },
+          miscDocuments: {
+            title: 'Documents annexes',
+            data: row.miscDocuments.data
+          }
+        },
+        name: row.auxiliary,
+        driveFolderLink: row.driveFolderLink
+      };
+      this.isOpened = true;
+      console.log(this.auxiliary);
+    },
+    goTo (url) {
+      openURL(url);
     }
   }
 };
