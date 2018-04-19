a6fa7e22e4be6148d3f816030dd5d72ebdc88e9f
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-04-19 15:31:54 +0200

    Add datatable to have record of SMS sent

--- src/api/Twilio.js
+++ src/api/Twilio.js
@@ -9,5 +9,9 @@ export default {
   async sendSMSConfirm (params, token) {
     const res = await axios.post(`${process.env.API_HOSTNAME}/twilio/sendConfirm/${params.phoneNbr}`, params, { headers: { 'x-access-token': token } });
     return res;
+  },
+  async getRecords (params, token) {
+    const res = await alenviAxios.get(`${process.env.API_HOSTNAME}/twilio/records`, { params });
+    return res;
   }
 }
--- src/pages/dashboard/auxiliaries/AddAuxiliary.vue
+++ src/pages/dashboard/auxiliaries/AddAuxiliary.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page padding>
-    <div class="row justify-center">
+    <div class="row justify-center q-mb-xl">
       <div style="width: 700px; max-width: 90vw;">
         <p class="caption">Création de compte Alenvi et Ogust</p>
         <select-sector v-model="selectedSector"></select-sector>
@@ -16,6 +16,31 @@
         <q-search v-model="searchUserFromMobilePhone" :debounce="600" placeholder="Numéro auxiliaire" type="tel" stack-label="Numéro de téléphone" /> -->
       </div>
     </div>
+    <p class="caption">Liste SMS envoyés</p>
+    <q-table
+      :data="filteredList"
+      :columns="columns"
+      row-key="name"
+      :rows-per-page-options="[20, 30, 40]"
+      :pagination.sync="pagination"
+      dense
+      :filter="filter"
+      rows-per-page-label="Lignes"
+      :pagination-label="paginationLabel"
+      no-data-label="Données non disponibles"
+      no-results-label="Pas de résultats"
+      :loading="tableLoading"
+      @request="getMessageList">
+      <template slot="top" slot-scope="props">
+        <q-search
+          hide-underline
+          color="primary"
+          v-model="filter"
+          clearable
+          placeholder="Recherche"
+          class="col-12" />
+      </template>
+    </q-table>
   </q-page>
 </template>
 
@@ -35,8 +60,49 @@ export default {
       employee: '',
       selectedSector: '',
       selectedManager: '',
-      isLoading: false
-    };
+      isLoading: false,
+      tableLoading: false,
+      pagination: {
+        page: 1,
+        rowsPerPage: 20,
+      },
+      filter: '',
+      messageList: [],
+      columns: [
+        {
+          name: 'dateSent',
+          required: true,
+          label: 'Date SMS accueil',
+          field: 'dateSent',
+          align: 'left',
+          sortable: true,
+          format (value) {
+            return new Date(value).toLocaleString([], {
+              day: '2-digit',
+              month: '2-digit',
+              year: 'numeric',
+              hour: '2-digit',
+              minute: '2-digit'
+            });
+          }
+        },
+        {
+          name: 'to',
+          required: true,
+          label: 'Destinataire',
+          field: 'to',
+          align: 'left'
+        }
+      ]
+    }
+  },
+  computed: {
+    filteredList () {
+      return this.messageList.filter(message => message.body.match(/Bienvenue/));
+    }
+  },
+  async mounted () {
+    await this.getMessageList();
   },
   methods: {
     async handlePhone () {
@@ -46,7 +112,6 @@ export default {
         // };
         // const res = await ogust.getEmployees(payload);
         // this.employee = res[0];
-        console.log('MANAGER', this.selectedManager);
         await this.$q.dialog({
           title: 'Confirmation accueil',
           message: `Es-tu sûr de vouloir envoyer un message au ${this.phoneNbr} ?`,
@@ -58,7 +123,6 @@ export default {
           mobile_phone: this.phoneNbr,
           sector: this.selectedSector,
           managerId: this.selectedManager,
-          firstSMS: Date.now()
         });
         const code = activationDataRaw.activationData.code;
         const message = await this.$twilio.sendSMS(this.phoneNbr, {
@@ -85,7 +149,49 @@ export default {
         });
         console.error(e);
       }
+    },
+    paginationLabel (start, end, total) {
+      return `${start} - ${end} de ${total}`;
+    },
+    async getMessageList () {
+      try {
+        this.tableLoading = true;
+        const messagesListRaw = await this.$twilio.getRecords();
+        this.messageList = messagesListRaw.data.data.messageList;
+        this.tableLoading = false;
+      } catch (e) {
+        console.error(e);
+        this.tableLoading = false;
+      }
     }
+    // async getMessageList ({ pagination }) {
+    //   try {
+    //     this.tableLoading = true;
+    //     const params = {};
+    //     params.pageSize = pagination.rowsNumber;
+    //     if (pagination.pageToken) {
+    //       params.pageToken = pagination.pageToken;
+    //     }
+    //     if (pagination.page > 1) {
+    //       params.pageNumber = pagination.page - 1;
+    //     }
+    //     const messagesListRaw = await this.$twilio.getRecords(params);
+    //     console.log(messagesListRaw);
+    //     this.pagination = pagination;
+    //     this.pagination.rowsNumber = Number(messagesListRaw.data.data.nextPage.PageSize);
+    //     console.log(this.pagination.rowsNumber);
+    //     this.pagination.page = Number(messagesListRaw.data.data.nextPage.Page) + 1;
+    //     console.log(this.pagination.page);
+    //     this.pagination.pageToken = messagesListRaw.data.data.nextPage.PageToken;
+    //     console.log(this.pagination.pageToken);
+    //     this.messageList = messagesListRaw.data.data.messageList;
+    //     console.log('MESSAGES', this.messageList);
+    //     this.tableLoading = false;
+    //   } catch (e) {
+    //     console.error(e);
+    //     this.tableLoading = false;
+    //   }
+    // }
   }
 }
 </script>
