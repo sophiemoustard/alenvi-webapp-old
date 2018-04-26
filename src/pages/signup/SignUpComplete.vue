0a61f432b92c5ecca8ba304989d5833bf3696c9f
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-04-26 14:54:16 +0200

    Change idCard prop to reflects API changes

--- src/pages/signup/SignUpComplete.vue
+++ src/pages/signup/SignUpComplete.vue
@@ -79,19 +79,19 @@
                 </q-field>
                 <p class="caption">Carte d'identité / titre de séjour (Recto) :</p>
                 <q-field icon="mdi-account-card-details" :error="$v.user.administrative.idCard.$error" error-label="Champ requis">
-                  <q-uploader v-if="storedUser && !storedUser.administrative.idCard && !storedUser.administrative.idCard.recto" name="idCardRecto" :url="docsUploadUrl" :headers="headers"
+                  <q-uploader v-if="storedUser && !storedUser.administrative.idCardRecto" name="idCardRecto" :url="docsUploadUrl" :headers="headers"
                   :additional-fields="[{ name: 'fileName', value: `cni_recto_${user.firstname}_${user.lastname}` }]"
                   @finish="afterUpload()" auto-expand hide-underline extensions="image/jpg, image/jpeg, image/gif, image/png, application/pdf"/>
-                  <p class="upload-done" v-if="storedUser && storedUser.administrative.idCard && storedUser.administrative.idCard.recto">Fichier mis en ligne <q-icon name="check" /></p>
-                  <p class="upload-not-done" v-if="storedUser && !storedUser.administrative.idCard && !storedUser.administrative.idCard.recto">Fichier manquant <q-icon name="warning" /></p>
+                  <p class="upload-done" v-if="storedUser && storedUser.administrative.idCardRecto">Fichier mis en ligne <q-icon name="check" /></p>
+                  <p class="upload-not-done" v-if="storedUser && !storedUser.administrative.idCardRecto">Fichier manquant <q-icon name="warning" /></p>
                 </q-field>
                 <p class="caption">Carte d'identité / titre de séjour (Verso) :</p>
                 <q-field icon="mdi-account-card-details" :error="$v.user.administrative.idCard.$error" error-label="Champ requis">
-                  <q-uploader v-if="storedUser && !storedUser.administrative.idCard && !storedUser.administrative.idCard.verso" name="idCardVerso" :url="docsUploadUrl" :headers="headers"
+                  <q-uploader v-if="storedUser && !storedUser.administrative.idCardVerso" name="idCardVerso" :url="docsUploadUrl" :headers="headers"
                   :additional-fields="[{ name: 'fileName', value: `cni_verso_${user.firstname}_${user.lastname}` }]"
                   @finish="afterUpload()" auto-expand hide-underline extensions="image/jpg, image/jpeg, image/gif, image/png, application/pdf"/>
-                  <p class="upload-done" v-if="storedUser && storedUser.administrative.idCard && storedUser.administrative.idCard.verso">Fichier mis en ligne <q-icon name="check" /></p>
-                  <p class="upload-not-done" v-if="storedUser && !storedUser.administrative.idCard && !storedUser.administrative.idCard.verso">Fichier manquant <q-icon name="warning" /></p>
+                  <p class="upload-done" v-if="storedUser && storedUser.administrative.idCardVerso">Fichier mis en ligne <q-icon name="check" /></p>
+                  <p class="upload-not-done" v-if="storedUser && !storedUser.administrative.idCardVerso">Fichier manquant <q-icon name="warning" /></p>
                 </q-field>
                 <p class="caption">Carte vitale :</p>
                 <q-field icon="mdi-account-card-details" :error="$v.user.administrative.vitalCard.$error" error-label="Champ requis">
