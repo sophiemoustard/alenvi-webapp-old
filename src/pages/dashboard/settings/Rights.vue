070a2d3aa4b519b46378d7726682e73c9cf17247
Pierre HOFMAN <pierre.hofman@epitech.eu>
2018-07-19 18:31:53 +0200

    Add rights update

--- src/pages/dashboard/settings/Rights.vue
+++ src/pages/dashboard/settings/Rights.vue
@@ -1,18 +1,39 @@
 <template>
   <q-page padding class="row justify-center">
     <div style="width: 1024px; max-width: 90vw;">
-      <p class="caption">Rôles existants:</p>
-      <table class="q-table striped-odd highlight compact">
-        <thead>
-          <tr>
-            <th class="text-center">Rôles \ Features</th>
-            <th class="text-center" v-for="(feature, index) in roles[0].features" :key="index">{{ feature.name }}</th>
-          </tr>
-        </thead>
+      <!-- <p class="caption">Rôles existants:</p> -->
+      <q-select
+        v-model="select"
+        float-label="Rôles existants"
+        radio
+        :options="selectOptions"
+        @input="changeRole"
+      />
+      <table class="q-table striped-odd highlight compact" v-if="roleChosen">
+        <!-- <thead> -->
+          <!-- @blur="updateRole(role, 1)" :readonly="isRoleAdmin(role)" -->
+          <!-- <td v-for="(role, index) in rolesTest" :key="index"><q-input type="text" align="center" v-model="role.name" readonly/></td> -->
+        <!-- </thead> -->
         <tbody>
+          <tr v-for="(right, index) in roleChosen.rights" :key="index">
+            <td class="text-center">{{ right.description }}</td>
+            <td class="text-center"><q-checkbox v-model="right.hasAccess" /></td>
+          </tr>
+          <!-- <tr v-for="(role, index) in rolesTest" :key="index">
+            <td class="text-center" v-for="(right, indexFeature) in role.rights" :key="indexFeature">
+              <q-input type="number" align="center" v-model="right.hasAccess" :readonly="isRoleAdmin(role)" @blur="updateRole(role, feature.permission_level)" />
+            </td> -->
+            <!-- <td class="text-center">
+              <div class="justify-center">
+                <q-btn icon="delete" color="primary" @click="deleteRole(role._id)" :disable="isRoleAdmin(role)" flat></q-btn>
+              </div>
+            </td> -->
+          <!-- </tr> -->
+        </tbody>
+        <!-- <tbody>
           <tr v-for="(role, index) in roles" :key="index">
             <td class="text-center">
-              <q-input type="text" align="center" v-model="role.name" readonly @blur="updateRole(role, 1)"/> <!-- :readonly="isRoleAdmin(role)" -->
+              <q-input type="text" align="center" v-model="role.name" readonly @blur="updateRole(role, 1)"/>
             </td>
             <td class="text-center" v-for="(feature, indexFeature) in role.features" :key="indexFeature">
               <q-input type="number" align="center" v-model.trim.number="feature.permission_level" :readonly="isRoleAdmin(role)" @blur="updateRole(role, feature.permission_level)" />
@@ -23,8 +44,9 @@
               </div>
             </td>
           </tr>
-        </tbody>
+        </tbody> -->
       </table>
+
       <div v-if="showRoleCreation" id="create-role">
         <br>
         <p class="caption">Nouveau rôle:</p>
@@ -66,6 +88,167 @@
 export default {
   data () {
     return {
+      select: {},
+      selectOptions: [],
+      roleChosen: {},
+      rolesTest: [
+        {
+          '_id': '5a3bc12a5e421400147d5eda',
+          'name': 'Coach',
+          'rights': [
+            {
+              'right_id': '5a3bc0005e421400147d5ec4',
+              'name': 'users:create',
+              'description': 'Créer utilisateurs',
+              'hasAccess': true
+            },
+            {
+              'right_id': '5a3bc0065e421400147d5ec7',
+              'name': 'profiles:read:user',
+              'description': 'Consulter les informations de profil d’autres utilisateurs',
+              'hasAccess': true,
+              'rolesConcerned': [{
+                'role_id': '5a3bc11f5e421400147d5ed5',
+                'name': 'Auxiliaire'
+              }]
+            },
+            {
+              'right_id': '5a3bc0315e421400147d5ecd',
+              'name': 'planning:edit:user',
+              'description': 'Editer les événements du planning des autres utilisateurs',
+              'hasAccess': true,
+              'rolesConcerned': [{
+                'role_id': '5a3bc11f5e421400147d5ed5',
+                'name': 'Auxiliaire'
+              }]
+            },
+            {
+              'right_id': '5a5735cb1f2a1f0014d48e14',
+              'name': 'planning:read:user',
+              'description': 'Voir les modifications planning des autres utilisateurs',
+              'hasAccess': true,
+              'rolesConcerned': [{
+                'role_id': '5a3bc11f5e421400147d5ed5',
+                'name': 'Auxiliaire'
+              }]
+            },
+            {
+              'right_id': '5a5735da1f2a1f0014d48e17',
+              'name': 'users:welcome:user',
+              'description': 'Processus d’accueil d’un utilisateur',
+              'hasAccess': true,
+              'rolesConcerned': [
+                {
+                  'role_id': '5a3bc11f5e421400147d5ed5',
+                  'name': 'Auxiliaire'
+                },
+                {
+                  'role_id': '5a573f4c1f2a1f0014d48e1a',
+                  'name': 'Aidant'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          '_id': '5a573f4c1f2a1f0014d48e1a',
+          'name': 'Client',
+          'rights': [
+            {
+              'right_id': '5a3bc0005e421400147d5ec4',
+              'name': 'users:create',
+              'description': 'Créer utilisateurs',
+              'hasAccess': false
+            },
+            {
+              'right_id': '5a3bc0065e421400147d5ec7',
+              'name': 'profiles:read:user',
+              'description': 'Consulter les informations de profil d’autres utilisateurs',
+              'hasAccess': false,
+              'rolesConcerned': []
+            },
+            {
+              'right_id': '5a3bc0315e421400147d5ecd',
+              'name': 'planning:edit:user',
+              'description': 'Editer les événements du planning des autres utilisateurs',
+              'hasAccess': false,
+              'rolesConcerned': []
+            },
+            {
+              'right_id': '5a5735cb1f2a1f0014d48e14',
+              'name': 'planning:read:user',
+              'description': 'Voir les modifications planning des autres utilisateurs',
+              'hasAccess': false,
+              'rolesConcerned': []
+            },
+            {
+              'right_id': '5a5735da1f2a1f0014d48e17',
+              'name': 'users:welcome:user',
+              'description': 'Processus d’accueil d’un utilisateur',
+              'hasAccess': false,
+              'rolesConcerned': []
+            }
+          ],
+        },
+        {
+          '_id': '5a3bc12f5e421400147d5edf',
+          'name': 'Auxiliaire',
+          'rights': [
+            {
+              'right_id': '5a3bc0005e421400147d5ec4',
+              'name': 'users:create',
+              'description': 'Créer utilisateurs',
+              'hasAccess': false
+            },
+            {
+              'right_id': '5a3bc0065e421400147d5ec7',
+              'name': 'profiles:read:user',
+              'description': 'Consulter les informations de profil d’autres utilisateurs',
+              'hasAccess': true,
+              'rolesConcerned': [{
+                'role_id': '5a3bc11f5e421400147d5ed5',
+                'name': 'Auxiliaire'
+              }]
+            },
+            {
+              'right_id': '5a3bc0315e421400147d5ecd',
+              'name': 'planning:edit:user',
+              'description': 'Editer les événements du planning des autres utilisateurs',
+              'hasAccess': false,
+              'rolesConcerned': [{
+                'role_id': '5a3bc11f5e421400147d5ed5',
+                'name': 'Auxiliaire'
+              }]
+            },
+            {
+              'right_id': '5a5735cb1f2a1f0014d48e14',
+              'name': 'planning:read:user',
+              'description': 'Voir les modifications planning des autres utilisateurs',
+              'hasAccess': true,
+              'rolesConcerned': [{
+                'role_id': '5a3bc11f5e421400147d5ed5',
+                'name': 'Auxiliaire'
+              }]
+            },
+            {
+              'right_id': '5a5735da1f2a1f0014d48e17',
+              'name': 'users:welcome:user',
+              'description': 'Processus d’accueil d’un utilisateur',
+              'hasAccess': true,
+              'rolesConcerned': [
+                {
+                  'role_id': '5a3bc11f5e421400147d5ed5',
+                  'name': 'Auxiliaire'
+                },
+                {
+                  'role_id': '5a573f4c1f2a1f0014d48e1a',
+                  'name': 'Aidant'
+                }
+              ]
+            }
+          ]
+        }
+      ],
       roleToAdd: {
         features: []
       },
@@ -80,11 +263,24 @@ export default {
   async created () {
     try {
       await this.getRoles();
+      this.fillSelect();
     } catch (e) {
       console.error(e);
     }
   },
   methods: {
+    changeRole (selected) {
+      for (let i = 0; i < this.rolesTest.length; i++) {
+        if (this.rolesTest[i]._id === selected) {
+          this.roleChosen = this.rolesTest[i];
+        }
+      }
+    },
+    fillSelect () {
+      for (let i = 0; i < this.rolesTest.length; i++) {
+        this.$set(this.selectOptions, i, { label: this.rolesTest[i].name, value: this.rolesTest[i]._id })
+      }
+    },
     async getRoles () {
       try {
         const rolesRaw = await this.$roles.showAll();
