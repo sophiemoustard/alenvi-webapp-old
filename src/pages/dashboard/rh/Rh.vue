84b5be0396916926bfda39893f321c20dba82911
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-08-08 11:20:22 +0200

    Add Rh page

---
+++ src/pages/dashboard/rh/Rh.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="neutral-background" padding>
+    <router-view />
+  </q-page>
+</template>
+
+<style lang="stylus" scoped>
+  @import "~variables"
+
+  .neutral-background
+    background: $grey-3
+
+</style>
