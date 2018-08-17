e4b9b7da40e1596ca5bc1f55829c1429552cce42
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-08-17 17:51:20 +0200

    Add SelectMentor component

---
+++ src/components/SelectMentor.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-select :value="value" :stack-label="stackLabel" ref="selectMentor" @change="updateMentor" :options="orderedMentors" separator />
+</template>
+
+<script>
+import _ from 'lodash';
+
+export default {
+  name: 'SelectMentor',
+  props: ['value', 'icon', 'stackLabel', 'sector'],
+  data () {
+    return {
+      mentors: null
+    };
+  },
+  async mounted () {
+    await this.getMentors();
+  },
+  computed: {
+    orderedMentors () {
+      return _.sortBy(this.mentors, ['value']);
+    }
+  },
+  methods: {
+    async getMentors () {
+      try {
+        const allMentorsRaw = await this.$users.showAll({ role: 'Auxiliaire', isActive: true, sector: this.sector });
+        this.mentors = allMentorsRaw.map(mentor => {
+          return {
+            label: `${mentor.firstname} ${mentor.lastname}`,
+            value: `${mentor.firstname} ${mentor.lastname}`
+          }
+        })
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    updateMentor (value) {
+      this.$emit('input', value);
+    }
+  }
+};
+</script>
