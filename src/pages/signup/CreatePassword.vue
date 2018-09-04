4ce20c5e2913b7048e5ce4283dc04c0245c466c5
Pierre HOFMAN <pierre.hofman@epitech.eu>
2018-09-04 17:40:28 +0200

    Add colors, validations, new fields, sizing on CreatePassoword

--- src/pages/signup/CreatePassword.vue
+++ src/pages/signup/CreatePassword.vue
@@ -1,49 +1,91 @@
 <template>
-  <div>
-    <div class="full-width row signup-header-padding">
+  <div class="neutral-background">
+    <div class="full-width row signup-header-padding bg-white">
       <div class="col-6">
         <img style="height: 30px" src="https://res.cloudinary.com/alenvi/image/upload/v1507124345/images/business/alenvi_logo_complet_full.png" alt="">
       </div>
-      <div class="col-6">Espace Auxiliaire d'envie</div>
+      <div class="col-6"><p style="font-size: 0.8rem">Espace Auxiliaire d'envie</p></div>
     </div>
     <div class="signup-body-padding">
       <div class="row justify-between items-baseline">
         <div class="col-2" style="text-align: left">
-          <span><q-icon name="clear" size="1rem" /></span>
+          <span><q-icon name="thumb up" size="2rem" /></span>
         </div>
         <div class="col-10">
-          <h5>Afin de confirmer la création de votre compte Alenvi, merci de <span class="text-weight-bold">choisir un mot de passe</span></h5>
+          <p style="font-size: 0.8rem">Afin de confirmer la création de votre compte Alenvi, merci de <span class="text-weight-bold">choisir un mot de passe</span></p>
         </div>
       </div>
       <div class="row margin-input">
         <div class="col-12">
           <div class="row justify-between">
-            <p class="input-caption">Message</p>
+            <p class="input-caption">Email</p>
+            <q-icon v-if="$v.user.alenvi.local.email.$error" name="error_outline" color="secondary" />
           </div>
-          <q-input v-model="message" type="textarea" rows="7"
-            color="white" inverted-light
-          />
+          <q-input v-model.trim="user.alenvi.local.email" color="white" inverted-light lower-case />
         </div>
       </div>
-      <div class="row justify-between">
-        <p class="input-caption">Adresse email</p>
-        <q-icon v-if="$v.user.alenvi.local.email.$error" name="error_outline" color="secondary" />
+      <div class="row margin-input">
+        <div class="col-12">
+          <div class="row justify-between">
+            <p class="input-caption">Mot de passe</p>
+            <q-icon v-if="$v.user.alenvi.local.password.$error" name="error_outline" color="secondary" />
+          </div>
+          <q-input v-model.trim="user.alenvi.local.password" type="password" color="white" inverted-light lower-case />
+        </div>
       </div>
-      <q-input v-model.trim="user.alenvi.local.email"
-        color="white"
-        inverted-light
-        lower-case
-        @focus="saveTmp('local.email')"
-        @blur="updateUser({ alenvi: 'local.email', ogust: 'email' })"
-      />
+      <div class="row margin-input">
+        <div class="col-12">
+          <div class="row justify-between">
+            <p class="input-caption">Confirmation mot de passe</p>
+            <q-icon v-if="$v.passwordConfirm.$error" name="error_outline" color="secondary" />
+          </div>
+          <q-input v-model.trim="passwordConfirm" type="password" color="white" inverted-light lower-case />
+        </div>
+      </div>
     </div>
     <q-btn no-caps class="full-width signup-btn" label="Envoyer message" icon-right="send" color="primary" :loading="loading" @click="sendMessage()" />
   </div>
 </template>
 
 <script>
-export default {
+import { mapGetters } from 'vuex';
+import { required, email, sameAs, minLength } from 'vuelidate/lib/validators';
 
+export default {
+  name: 'CreatePassword',
+  data () {
+    return {
+      loading: false,
+      user: {
+        alenvi: {
+          local: {
+            email: 'pierre.hofman@gmail.com',
+            password: ''
+          }
+        }
+      },
+      passwordConfirm: ''
+    }
+  },
+  validations () {
+    return {
+      user: {
+        alenvi: {
+          local: {
+            email: { required, email },
+            password: { required, minLength: minLength(4) }
+          }
+        }
+      },
+      passwordConfirm: { required, sameAsPassword: sameAs('password') }
+    }
+  },
+  computed: {
+    ...mapGetters({
+      // currentUser: 'main/user',
+      // user: 'rh/getUserProfile'
+    })
+  }
 }
 </script>
 
@@ -51,7 +93,7 @@ export default {
 
   .signup
     &-header-padding
-      padding: 24px 24px 24px 24px
+      padding: 24px 24px 12px 24px
     &-body-padding
       padding: 0px 24px 0px 24px
     &-btn
@@ -61,4 +103,7 @@ export default {
     margin-bottom: 6px
     &.last
       margin-bottom: 24px
+
+  .bg-main
+    background:
 </style>
