55df9af178d57783f4a3439b29b5d07d44a1a05f
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-09-05 19:18:36 +0200

    Save task notifications when dispatching getUserProfile action

--- src/store/rh/actions.js
+++ src/store/rh/actions.js
@@ -5,13 +5,24 @@ export function someAction (context) {
 
 import users from '../../api/Users'
 import { userProfileValidation } from '../../helpers/userProfileValidation';
+import { taskValidation } from '../../helpers/taskValidation';
 
 export async function getUserProfile ({ commit }, userId) {
   try {
     const user = await users.getById(userId);
-    const userValidation = userProfileValidation(user);
     commit('saveUserProfile', user);
-    commit('saveNotification', { type: 'profiles', _id: user._id, count: userValidation.error ? userValidation.error.details.length : 0 });
+    const userValidation = userProfileValidation(user);
+    commit('saveNotification', {
+      type: 'profiles',
+      _id: user._id,
+      count: userValidation.error ? userValidation.error.details.length : 0
+    });
+    const checkTasks = user.procedure.filter(task => taskValidation(task, user).length > 0 && !task.check.isDone);
+    commit('saveNotification', {
+      type: 'tasks',
+      _id: user._id,
+      count: checkTasks.length > 0 ? checkTasks.length : 0
+    });
   } catch (e) {
     console.error(e);
   }
