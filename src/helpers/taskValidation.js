fddc89f716c503a8e581d67e2beb5a9fa2972f1b
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-09-05 19:17:56 +0200

    Add taskValidation helpers

---
+++ src/helpers/taskValidation.js
@@ -0,0 +1,54 @@
+import euSpace from '../data/euSpace';
+import users from '../api/Users';
+
+const nationalityValidation = (userNationality) => {
+  if (!userNationality) return;
+  return Object.keys(euSpace).indexOf(userNationality);
+}
+
+export const taskValidation = (task, user = null) => {
+  const error = [];
+  if (task.task.name.match(/titre de séjour/i)) {
+    if (user.administrative && user.administrative.identity) {
+      if (nationalityValidation(user.administrative.identity.nationality) === -1) {
+        error.push({
+          detail: 'Nationality validation',
+          msg: 'Not from EU.'
+        })
+      }
+    }
+  }
+  if (task.task.name.match(/attribuer parrain/i)) {
+    if (!user.mentorId) {
+      error.push({
+        detail: 'Mentor validation',
+        msg: 'No mentor.'
+      })
+    }
+  }
+  return error;
+};
+
+export const displayTask = (task, user = null) => {
+  if (task.task.name.match(/titre de séjour/i)) {
+    if (user.administrative && user.administrative.identity) {
+      if (nationalityValidation(user.administrative.identity.nationality) === -1) {
+        return true;
+      }
+    }
+  }
+  if (task.task.name.match(/attribuer parrain/i)) {
+    return true;
+  }
+  return false;
+};
+
+export const autoCheckTask = async (task, user = null) => {
+  if (task.task.name.match(/attribuer parrain/i) && user.mentorId) {
+    console.log('MEH');
+    await users.updateTask({ user_id: user._id, task_id: task.task._id, isDone: true });
+    return true;
+  }
+  console.log('PEH');
+  return false;
+};
