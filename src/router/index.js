789007add11fff7e12c598b8e707db755b570a80
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-09-07 16:04:30 +0200

    No need of wrapping router in a function because not using SSR for now

--- src/router/index.js
+++ src/router/index.js
@@ -16,67 +16,69 @@ Vue.use(VueMeta);
  * directly export the Router instantiation
  */
 
-export default function (/* { store, ssrContext } */) {
-  const Router = new VueRouter({
-    scrollBehavior: () => ({ y: 0 }),
-    routes,
+// export default function (/* { store, ssrContext } */) {
 
-    // Leave these as is and change from quasar.conf.js instead!
-    // quasar.conf.js -> build -> vueRouterMode
-    mode: process.env.VUE_ROUTER_MODE,
-    base: process.env.VUE_ROUTER_BASE
-  })
+//   return Router
+// }
+const Router = new VueRouter({
+  scrollBehavior: () => ({ y: 0 }),
+  routes,
 
-  /*
+  // Leave these as is and change from quasar.conf.js instead!
+  // quasar.conf.js -> build -> vueRouterMode
+  mode: process.env.VUE_ROUTER_MODE,
+  base: process.env.VUE_ROUTER_BASE
+})
+
+/*
 // Inform Google Analytics
 Router.beforeEach((to, from, next) => {
-  if (typeof ga !== 'undefined') {
-    ga('set', 'page', to.path)
-    ga('send', 'pageview')
-  }
-  next()
+if (typeof ga !== 'undefined') {
+  ga('set', 'page', to.path)
+  ga('send', 'pageview')
+}
+next()
 })
 */
-  Router.beforeEach(async (to, from, next) => {
-    if (to.meta.cookies) {
-      if (!Cookies.get('alenvi_token') || !Cookies.get('user_id')) {
-        if (await alenvi.refreshAlenviCookies()) {
-          if (store.state.main.refreshState) {
-            await store.dispatch('main/getUser', Cookies.get('user_id'));
-          }
-          if (await checkPermission(to, store.getters['main/user'])) {
-            store.commit('main/changeRefreshState', false);
-            next();
-          } else {
-            if (store.getters['main/user'].role.name === 'Client') {
-              store.commit('main/changeRefreshState', false);
-              return next({ path: '/dashboard/customer/home' });
-            }
-            store.commit('main/changeRefreshState', false);
-            next({ path: '/dashboard' });
-          }
-        } else {
-          next({ path: '/login', query: { from: to.path } });
-        }
-      } else {
+Router.beforeEach(async (to, from, next) => {
+  if (to.meta.cookies) {
+    if (!Cookies.get('alenvi_token') || !Cookies.get('user_id')) {
+      if (await alenvi.refreshAlenviCookies()) {
         if (store.state.main.refreshState) {
           await store.dispatch('main/getUser', Cookies.get('user_id'));
         }
         if (await checkPermission(to, store.getters['main/user'])) {
           store.commit('main/changeRefreshState', false);
           next();
-        } else if (store.getters['main/user'].role.name === 'Client') {
-          store.commit('main/changeRefreshState', false);
-          next({ path: '/dashboard/customer/home' });
         } else {
+          if (store.getters['main/user'].role.name === 'Client') {
+            store.commit('main/changeRefreshState', false);
+            return next({ path: '/dashboard/customer/home' });
+          }
           store.commit('main/changeRefreshState', false);
           next({ path: '/dashboard' });
         }
+      } else {
+        next({ path: '/login', query: { from: to.path } });
       }
     } else {
-      next();
+      if (store.state.main.refreshState) {
+        await store.dispatch('main/getUser', Cookies.get('user_id'));
+      }
+      if (await checkPermission(to, store.getters['main/user'])) {
+        store.commit('main/changeRefreshState', false);
+        next();
+      } else if (store.getters['main/user'].role.name === 'Client') {
+        store.commit('main/changeRefreshState', false);
+        next({ path: '/dashboard/customer/home' });
+      } else {
+        store.commit('main/changeRefreshState', false);
+        next({ path: '/dashboard' });
+      }
     }
-  })
+  } else {
+    next();
+  }
+});
 
-  return Router
-}
+export default Router;
