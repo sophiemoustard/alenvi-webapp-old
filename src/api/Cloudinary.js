4a3b7fa084d313221feba21e08bc1424cd61fa90
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-09-10 15:57:13 +0200

    Add cloudinary api request

---
+++ src/api/Cloudinary.js
@@ -0,0 +1,8 @@
+import { alenviAxios } from './ressources/alenviAxios';
+
+export default {
+  async deleteImageById (params) {
+    const file = await alenviAxios.delete(`${process.env.API_HOSTNAME}/cloudinary/image/${encodeURIComponent(params.id)}`);
+    return file;
+  }
+}
