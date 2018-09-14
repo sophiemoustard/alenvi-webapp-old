662f1ff6e68c107f50d64d693bedbdf5835c6d80
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-09-14 18:58:31 +0200

    Add customImg component

---
+++ src/components/CustomImg.vue
@@ -0,0 +1,34 @@
+<template>
+  <img :src="link" :alt="alt" width="150px" height="150px">
+</template>
+
+<script>
+import gdrive from '../api/GoogleDrive';
+export default {
+  name: 'custom-img',
+  props: {
+    driveId: {
+      type: String,
+      required: true
+    },
+    alt: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      link: ''
+    }
+  },
+  mounted () {
+    this.getThumbnailUrl()
+  },
+  methods: {
+    async getThumbnailUrl () {
+      const file = await gdrive.getFileById({ id: this.driveId });
+      this.link = file.data.data.file.thumbnailLink;
+    }
+  }
+}
+</script>
