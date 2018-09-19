58fea1ab53832a07dba8f84c7564053780cf33b8
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-09-19 17:29:02 +0200

    Update checkPermission to reflects changes regarding user rights

--- src/helpers/checkPermission.js
+++ src/helpers/checkPermission.js
@@ -3,15 +3,20 @@ import redirect from '../router/redirect'
 export const checkPermission = async (to, user) => {
   try {
     if (to.meta.permission) {
-      if (user.role && user.role.features.length !== 0) {
-        for (let i = 0, l = user.role.features.length; i < l; i++) {
-          if (user.role.features[i].name === to.meta.permission) {
-            return user.role.features[i].permission_level > 0;
-          }
-        }
+      if (user.role && user.role.rights) {
+        return user.role.rights.filter(right => right === to.meta.permission).length === 1;
       } else {
         return redirect.redirectToLogin({ to });
       }
+      // if (user.role && user.role.features.length !== 0) {
+      //   for (let i = 0, l = user.role.features.length; i < l; i++) {
+      //     if (user.role.features[i].name === to.meta.permission) {
+      //       return user.role.features[i].permission_level > 0;
+      //     }
+      //   }
+      // } else {
+      //   return redirect.redirectToLogin({ to });
+      // }
     }
     return true;
   } catch (e) {
