66de689c4aa98062ef6ef5e92ebda24afe129871
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-09-21 09:48:27 +0200

    Add watcher for userProfile

--- src/pages/dashboard/rh/directory/profile/Profile.vue
+++ src/pages/dashboard/rh/directory/profile/Profile.vue
@@ -51,6 +51,11 @@ export default {
   mounted () {
     this.$store.dispatch('rh/getUserProfile', this.id);
   },
+  watch: {
+    userProfile (value) {
+      this.$store.dispatch('rh/updateNotifications', value);
+    }
+  },
   beforeDestroy () {
     this.$store.commit('rh/saveUserProfile', null);
   }
