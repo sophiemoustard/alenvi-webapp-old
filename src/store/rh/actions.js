f3b86987efd93bb11d4f72d5be0be80a3f192c8e
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-09-21 09:49:03 +0200

    Split getUserProfile into another action to handle notifications

--- src/store/rh/actions.js
+++ src/store/rh/actions.js
@@ -12,19 +12,24 @@ export async function getUserProfile ({ commit }, userId) {
     const user = await users.getById(userId);
     user.mobilePhone = user.mobilePhone.split(' ').join('');
     commit('saveUserProfile', user);
-    const userValidation = userProfileValidation(user);
-    commit('saveNotification', {
-      type: 'profiles',
-      _id: user._id,
-      count: userValidation.error ? userValidation.error.details.length : 0
-    });
-    const checkTasks = user.procedure.filter(task => taskValidation(task, user).length > 0 && !task.check.isDone);
-    commit('saveNotification', {
-      type: 'tasks',
-      _id: user._id,
-      count: checkTasks.length > 0 ? checkTasks.length : 0
-    });
   } catch (e) {
     console.error(e);
   }
-};
+}
+
+export function updateNotifications ({ commit, state }) {
+  // console.log('MEH', user.administrative.identityDocs);
+  const user = state.userProfile;
+  const userValidation = userProfileValidation(user);
+  commit('saveNotification', {
+    type: 'profiles',
+    _id: user._id,
+    count: userValidation.error ? userValidation.error.details.length : 0
+  });
+  const checkTasks = user.procedure.filter(task => taskValidation(task, user).length > 0 && !task.check.isDone);
+  commit('saveNotification', {
+    type: 'tasks',
+    _id: user._id,
+    count: checkTasks.length > 0 ? checkTasks.length : 0
+  });
+}
--- src/store/rh/mutations.js
+++ src/store/rh/mutations.js
@@ -2,9 +2,13 @@
 export function someMutation (state) {
 }
 */
-
 export function saveUserProfile (state, data) {
-  state.userProfile = data;
+  state.userProfile = Object.assign({}, data);
+}
+
+export function saveUserProfileByField (state, data) {
+  // state.userProfile = _.set(state.userProfile, data.path, data.value)
+  state.userProfile = Object.assign({}, data);
 }
 
 export function saveNotification (state, notification) {
