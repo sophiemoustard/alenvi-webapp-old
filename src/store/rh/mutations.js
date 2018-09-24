a6617e3ac5c32aed86a78f9bc9bd4b9e9a7a21fc
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-09-24 19:04:45 +0200

    Handle case when sending null to userProfile

--- src/store/rh/mutations.js
+++ src/store/rh/mutations.js
@@ -3,12 +3,8 @@ export function someMutation (state) {
 }
 */
 export function saveUserProfile (state, data) {
-  state.userProfile = Object.assign({}, data);
-}
-
-export function saveUserProfileByField (state, data) {
-  // state.userProfile = _.set(state.userProfile, data.path, data.value)
-  state.userProfile = Object.assign({}, data);
+  // state.userProfile = Object.assign({}, data);
+  state.userProfile = !data ? data : Object.assign({}, data);
 }
 
 export function saveNotification (state, notification) {
