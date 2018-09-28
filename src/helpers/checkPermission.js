9cf4f1037e37ccd2a11e6ad54c713224d89f2594
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-09-28 17:21:31 +0200

    Update checkPermission to handle multiple permissions

--- src/helpers/checkPermission.js
+++ src/helpers/checkPermission.js
@@ -1,10 +1,22 @@
-import redirect from '../router/redirect'
+import { Cookies } from 'quasar';
+
+import redirect from '../router/redirect';
+
+const findPermission = (right, paramsId) => {
+  return (permission) => {
+    if (typeof permission === 'string') {
+      return permission === right.permission;
+    } else if (paramsId && typeof permission.name === 'string' && typeof permission.when === 'function') {
+      return permission.name === right.permission && permission.when(paramsId, Cookies.get('user_id'));
+    }
+  }
+}
 
 export const checkPermission = async (to, user) => {
   try {
-    if (to.meta.permission) {
+    if (to.meta.permissions) {
       if (user.role && user.role.rights) {
-        return user.role.rights.filter(right => right.permission === to.meta.permission).length === 1;
+        return user.role.rights.filter(right => to.meta.permissions.find(findPermission(right, to.params.id || null))).length > 0;
       } else {
         return redirect.redirectToLogin({ to });
       }
