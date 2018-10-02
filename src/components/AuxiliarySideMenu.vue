686e12d9bc5e1ddf6d15adc1bc39545b2574958f
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-10-02 11:52:07 +0200

    Add account icon in auxiliary sidemenu footer

--- src/components/AuxiliarySideMenu.vue
+++ src/components/AuxiliarySideMenu.vue
@@ -57,10 +57,10 @@
     <div class="sidemenu-footer">
       <q-item class="sidemenu-footer-border full-width">
         <q-item-main class="sidemenu-footer-user" :label="userFirstnameUpper" />
-        <!-- <q-item-side>
+        <q-item-side>
           <q-icon name="person" color="black" />
         </q-item-side>
-        <q-item-side>
+        <!-- <q-item-side>
           <q-icon name="settings" color="black" />
         </q-item-side> -->
         <q-item-side>
