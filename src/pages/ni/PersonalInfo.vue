c3939b1f47c8ab9eef1a7f2a4d7dad8cfb98b70d
Pierre HOFMAN <pierre.hofman@gmail.com>
2018-10-05 15:53:43 +0200

    Update title

--- src/pages/ni/PersonalInfo.vue
+++ src/pages/ni/PersonalInfo.vue
@@ -2,7 +2,7 @@
   <q-page padding v-if="userProfile" class="neutral-background">
     <profile-header v-if="currentUser.role.name !== 'Auxiliaire'" :profileId="id" class="header-margin" />
     <profile-tabs v-if="currentUser.role.name !== 'Auxiliaire'" :profileId="id" :tabsContent="tabsContent" />
-    <h4 v-if="currentUser.role.name === 'Auxiliaire'">Mettre à jour mes informations personnelles</h4>
+    <h4 v-if="currentUser.role.name === 'Auxiliaire'">Informations personnelles</h4>
     <profile-info v-if="currentUser.role.name === 'Auxiliaire'" />
   </q-page>
 </template>
