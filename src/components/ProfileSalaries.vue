499293d88f6431ff34a69ccc8b118ebdc0a4595b
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-11-05 10:56:34 +0100

    Add logic to profile salaries (WIP)

--- src/components/ProfileSalaries.vue
+++ src/components/ProfileSalaries.vue
@@ -4,23 +4,46 @@
 </template>
 
 <script>
+import moment from 'moment-business-days';
+import Holidays from 'date-holidays';
+
+const holidays = new Holidays('FR');
+const date = new Date();
+const currentYear = date.getFullYear();
+const currentHolidays = holidays.getHolidays(currentYear);
+moment.locale('fr', {
+  holidays: currentHolidays.map(holiday => holiday.date),
+  holidayFormat: 'YYYY-MM-DD HH:mm:ss',
+  workingWeekdays: [1, 2, 3, 4, 5, 6]
+});
+
 export default {
+  props: {
+    id: String
+  },
   data () {
     return {
+      user: null,
+      contracts: null,
+      company: null,
+      holidays: null
     }
   },
   computed: {
-    getUser () {
-      return this.$store.getters['rh/getUserProfile'];
+    usePublicTransport () {
+      return this.user.administrative.transportInvoice.transportType;
+    },
+    hasPhoneInvoice () {
+      return this.user.administrative.phoneInvoice.driveId;
     }
   },
   async mounted () {
-    try {
-      const user = await this.$users.getById(this.getUser._id);
-      console.log(user);
-    } catch (e) {
-      console.error(e);
-    }
+    this.user = await this.$users.getById(this.id);
+    this.holidays = currentHolidays;
+    console.log(this.holidays);
+    this.contracts = this.user.administrative.contracts;
+    this.company = this.user.company;
+    console.log(moment().monthBusinessDays());
   },
   methods: {
   }
