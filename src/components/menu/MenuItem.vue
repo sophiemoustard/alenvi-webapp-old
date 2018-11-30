000becf20167382a56e2b54eecb1f4331e1f8d4d
sophiemoustard <sophie.moustard@gmail.com>
2018-11-30 10:13:46 +0100

    Create MenuItem component + clean sideMenuCoach

---
+++ src/components/menu/MenuItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-item v-if="displayItem" :to="{ name, params, query }" exact>
+    <q-item-side :icon="icon" />
+    <q-item-main :label="label" />
+  </q-item>
+</template>
+
+<script>
+export default {
+  props: {
+    params: Object,
+    query: Object,
+    name: String,
+    label: String,
+    icon: String,
+    displayItem: { type: Boolean, default: true },
+  },
+}
+</script>
