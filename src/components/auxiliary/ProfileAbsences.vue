529d5fc06ae77a854952000a9d092d109369abf6
sophiemoustard <sophie.moustard@gmail.com>
2018-12-04 15:39:39 +0100

    Remove comment and console log in ProfileAbsence

--- src/components/auxiliary/ProfileAbsences.vue
+++ src/components/auxiliary/ProfileAbsences.vue
@@ -22,33 +22,12 @@
           </q-td>
           <q-td slot="body-cell-delete" slot-scope="props" :props="props">
             <q-btn flat round small color="grey" icon="delete" @click.native="removeAbsence(props.value, props.row.__index)" />
-            <!-- <q-icon class="cursor-pointer" color="grey" name="delete" @click.native="remove(props.value.id, props.row.__index, props.value.userId)" size="1.5rem" /> -->
           </q-td>
         </q-table>
       </q-card-main>
     </q-card>
-
-    <!-- <div class="row margin-input">
-      <div class="col-xs-12">
-        <div class="row justify-between">
-          <p class="input-caption">Justificatif</p>
-          <q-icon v-if="$v.newAbsence.document.$error" error-label="Champ requis" />
-        </div>
-        <div class="row justify-center">
-            <q-uploader ref="absenceReason" name="absenceReason" :url="docsUploadUrl" :headers="headers"
-              :additional-fields="[
-                { name: 'fileName', value: `justificatif_absence_${getUser.firstname}_${getUser.lastname}_${this.$moment().format('DD-MM-YYYY')}` },
-                { name: 'absenceId', value: contract._id },
-                { name: 'versionId', value: props.row._id }
-              ]"
-              hide-underline extensions="image/jpg, image/jpeg, image/gif, image/png, application/pdf"
-              hide-upload-button @add="uploadDocument($event, `signedContract_${props.row._id}`)" @uploaded="refreshUser" @fail="failMsg" />
-          </div>
-      </div>
-    </div> -->
     <q-btn class="fixed fab-add-person" no-caps rounded color="primary" icon="ion-document" label="Enregistrer une absence"
       @click="newAbsenceModal = true" />
-    <!-- <p></p> -->
     <q-modal v-model="newAbsenceModal" :content-css="modalCssContainer">
       <div class="modal-padding">
         <div class="row justify-between items-baseline">
@@ -92,7 +71,7 @@
             </div>
             <q-field :error="$v.newAbsence.startDuration.$error" error-label="Champ requis">
               <q-select :options="dateOptions" v-model="newAbsence.startDuration" color="white" inverted-light
-                separator /> <!-- @blur="$v.newAbsenceOptionsSelected.startDateOption.$touch"  -->
+                separator />
             </q-field>
           </div>
         </div>
@@ -105,7 +84,6 @@
             <q-field :error="$v.newAbsence.endDate.$error" error-label="Champ requis">
               <q-datetime :disable="!newAbsence.startDate" type="date" format="DD/MM/YYYY" v-model="newAbsence.endDate"
                 color="white" inverted-light popover ok-label="OK" cancel-label="Fermer" />
-              <!-- :min="newAbsence.startDate" -->
             </q-field>
           </div>
         </div>
@@ -118,7 +96,6 @@
             <q-field :error="$v.newAbsence.endDuration.$error" error-label="Champ requis">
               <q-select :disable="!newAbsence.endDate || newAbsence.endDate <= newAbsence.startDate" :options="dateOptions"
                 v-model="newAbsence.endDuration" color="white" inverted-light separator />
-              <!-- @blur="$v.newAbsenceOptionsSelected.startDateOption.$touch"  -->
             </q-field>
           </div>
         </div>
@@ -293,7 +270,6 @@ export default {
     try {
       const user = await this.$users.getById(this.getUser._id);
       this.absences = user.administrative.absences;
-      console.log(this.absences);
     } catch (e) {
       console.error(e);
     }
@@ -312,7 +288,6 @@ export default {
           method: 'POST',
           data: this.newAbsence
         });
-        console.log(test);
       } catch (e) {
         console.error(e);
       } finally {
@@ -347,8 +322,6 @@ export default {
       }
     },
     uploadDocument (files, refName) {
-      console.log(refName)
-      console.log(this.$refs[refName]);
       if (files[0].size > 5000000) {
         this.$refs[refName].reset();
         this.$q.notify({
@@ -360,7 +333,6 @@ export default {
         });
         return '';
       } else {
-        console.log(this.$refs);
         this.$refs[refName].upload();
       }
     },
@@ -445,8 +417,6 @@ export default {
   /deep/ .q-uploader-pick-button
     color: $primary
     font-size: 1.5rem
-    // position: relative !important
     cursor: pointer !important
-    // background: blue
 
 </style>
