2c045a4c632e828c859614d87fdddda4ab2aaddb
Sophie Moustard <sophie.moustard@gmail.com>
2018-12-19 17:28:14 +0100

    Retours guillaume (#39)
    * Montant => Prix
    
    * Add hour unit
    
    * Auto complete unitTTCRAte

--- src/pages/ni/config/CustomersConfig.vue
+++ src/pages/ni/config/CustomersConfig.vue
@@ -62,7 +62,7 @@
         <ni-modal-input caption="Nom" v-model="newService.name" :error="$v.newService.name.$error" @blur="$v.newService.name.$touch" />
         <ni-modal-select caption="Nature" v-model="newService.nature" :error="$v.newService.nature.$error" @blur="$v.newService.nature.$touch"
           :options="natureOptions" />
-        <ni-modal-input caption="Montant unitaire par défaut TTC" suffix="€" type="number" v-model="newService.defaultUnitAmount"
+        <ni-modal-input caption="Prix unitaire par défaut TTC" suffix="€" type="number" v-model="newService.defaultUnitAmount"
           :error="$v.newService.defaultUnitAmount.$error" @blur="$v.newService.defaultUnitAmount.$touch"/>
         <ni-modal-input caption="TVA" suffix="%" v-model="newService.vat" type="number" :error="$v.newService.vat.$error" @blur="$v.newService.vat.$touch" />
         <ni-modal-input caption="Majoration dimanche/jours fériés" suffix="%" type="number" v-model="newService.holidaySurcharge" />
@@ -135,7 +135,7 @@ export default {
         },
         {
           name: 'defaultUnitAmount',
-          label: 'Montant unitaire par défaut TTC',
+          label: 'Prix unitaire par défaut TTC',
           align: 'center',
           field: row => `${row.defaultUnitAmount}€`,
           sortable: true,
