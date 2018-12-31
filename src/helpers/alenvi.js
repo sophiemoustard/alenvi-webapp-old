53ed0f425c1598e3d9f34db7a0a2f16cf06852fc
sophiemoustard <sophie.moustard@gmail.com>
2018-12-31 11:51:20 +0100

    Clean helpers

--- src/helpers/alenvi.js
+++ src/helpers/alenvi.js
@@ -8,11 +8,24 @@ export default {
         const data = {};
         data.refreshToken = Cookies.get('refresh_token');
         const newToken = await axios.post(`${process.env.API_HOSTNAME}/users/refreshToken`, data);
-        Cookies.set('alenvi_token', newToken.data.data.token, { path: '/', expires: date.addToDate(new Date(), { seconds: newToken.data.data.expiresIn }), secure: process.env.NODE_ENV !== 'development' });
-        Cookies.set('alenvi_token_expires_in', newToken.data.data.expiresIn, { path: '/', expires: date.addToDate(new Date(), { seconds: newToken.data.data.expiresIn }), secure: process.env.NODE_ENV !== 'development' });
-        Cookies.set('user_id', newToken.data.data.user._id, { path: '/', expires: date.addToDate(new Date(), { seconds: newToken.data.data.expiresIn }), secure: process.env.NODE_ENV !== 'development' });
+        Cookies.set(
+          'alenvi_token',
+          newToken.data.data.token,
+          { path: '/', expires: date.addToDate(new Date(), { seconds: newToken.data.data.expiresIn }), secure: process.env.NODE_ENV !== 'development' }
+        );
+        Cookies.set(
+          'alenvi_token_expires_in',
+          newToken.data.data.expiresIn,
+          { path: '/', expires: date.addToDate(new Date(), { seconds: newToken.data.data.expiresIn }), secure: process.env.NODE_ENV !== 'development' }
+        );
+        Cookies.set(
+          'user_id',
+          newToken.data.data.user._id,
+          { path: '/', expires: date.addToDate(new Date(), { seconds: newToken.data.data.expiresIn }), secure: process.env.NODE_ENV !== 'development' }
+        );
         return true;
       }
+
       Cookies.remove('alenvi_token', { path: '/' });
       Cookies.remove('user_id', { path: '/' });
       Cookies.remove('alenvi_token_expires_in', { path: '/' });
