74dc6259eac1824708e1fe78d8c79fd874419c65
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2019-01-03 16:32:14 +0100

    Remove cloneDeep as it is used in customerProfileValidation.js

--- src/helpers/getCustomerLastMandateOrQuote.js
+++ src/helpers/getCustomerLastMandateOrQuote.js
@@ -1,8 +1,5 @@
-import _ from 'lodash';
-
 export const getCustomerLastMandateOrQuote = (docs) => {
   if (!docs || !Array.isArray(docs) || docs.length === 0) return [];
-  const docsCopy = _.cloneDeep(docs);
-  const sortedDocs = docsCopy.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+  const sortedDocs = docs.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
   return [sortedDocs[0]];
 }
