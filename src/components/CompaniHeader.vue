5d6af2647666b8c04f8fbfd071cb29d77d68d178
Pierre HOFMAN <pierre.hofman@epitech.eu>
2019-01-07 15:13:44 +0100

    Replaced all logos (#82)

--- src/components/CompaniHeader.vue
+++ src/components/CompaniHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row justify-between signup-header-padding bg-white items-center col-12">
-    <img style="height: 30px" src="https://res.cloudinary.com/alenvi/image/upload/v1539605431/images/business/Compani/compani_logo_1L_RVB_S.png" alt="Logo Compani">
+    <img style="height: 30px" src="https://res.cloudinary.com/alenvi/image/upload/v1546865717/images/business/Compani/compani_texte_rose_1000.png" alt="Logo Compani">
     <p class="no-margin" style="font-size: 0.8rem; text-align: right">Espace Alenvi</p>
   </div>
 </template>
