8eaad12e482c3a81eb7cfbe8b689fb9d2f512319
Pierre HOFMAN <pierre.hofman@epitech.eu>
2019-01-15 17:59:50 +0100

    Add lib 'vue-smooth-dnd' to test, add native HTML5 API d&d, add testing case from an img

--- src/pages/proto/planning.vue
+++ src/pages/proto/planning.vue
@@ -1,5 +1,11 @@
 <template>
   <q-page padding class="neutral-background">
+    <div id="div1" class="mydiv" @drop="drop" @dragover.prevent>
+      <img src="https://cdn.sstatic.net/Sites/stackoverflow/company/img/logos/so/so-logo.png?v=9c558ec15d8a" draggable @dragstart="drag" id="drag1" width="88" height="31">
+    </div>
+    <div id="div2" class="mydiv" @drop="drop" @dragover.prevent></div>
+    <div id="div3" class="mydiv" @drop="drop" @dragover.prevent></div>
+    <div id="div4" class="mydiv" @drop="drop" @dragover.prevent></div>
     <p class="input-caption">Communauté</p>
     <ni-select-sector class="q-mb-md" @input="getEmployeesBySector" v-model="selectedSector" />
     <div class="planning-container full-width q-pa-md">
@@ -9,28 +15,30 @@
         <q-btn icon="chevron_right" flat round @click="goToNextWeek"></q-btn>
       </div>
       <table style="width: 100%">
-        <tr>
-          <td></td>
-          <td class="capitalize" v-for="(day, index) in daysHeader" :key="index">
-            {{day}}
-          </td>
-        </tr>
-        <tr class="auxiliaries-row" v-for="(auxiliary, index) in auxiliaries" :key="index">
-          <td>
-            {{auxiliary.firstname}} {{auxiliary.lastname}}
-          </td>
-          <td v-for="(day, dayIndex) in days" :key="dayIndex" valign="top" class="event-cell">
-            <div class="row cursor-pointer" v-for="(event, eventIndex) in getAuxiliaryEvents(auxiliary, dayIndex)" :key="eventIndex" @click="openEditionModal(event)">
-              <div class="col-12 event">
-                <p class="no-margin">{{ getEventHours(event) }}</p>
-                <p v-if="event.type === INTERVENTION" class="no-margin">{{ event.customer.identity.title }} {{ event.customer.identity.lastname }}</p>
-                <p v-if="event.type === ABSENCE">{{ displayAbsenceType(event.absence) }}</p>
-                <p v-if="event.type === UNAVAILABILITY">Indisponibilité</p>
-                <p v-if="event.type === INTERNAL_HOUR">{{ event.internalHour.name }}</p>
+        <tbody>
+          <tr>
+            <td></td>
+            <td class="capitalize" v-for="(day, index) in daysHeader" :key="index">
+              {{day}}
+            </td>
+          </tr>
+          <tr class="auxiliaries-row" v-for="(auxiliary, index) in auxiliaries" :key="index">
+            <td>
+              {{auxiliary.firstname}} {{auxiliary.lastname}}
+            </td>
+            <td @drop="drop" @dragover.prevent v-for="(day, dayIndex) in days" :key="dayIndex" valign="top" class="event-cell">
+              <div class="row cursor-pointer" v-for="(event, eventIndex) in getAuxiliaryEvents(auxiliary, dayIndex)" :key="eventIndex" @click="openEditionModal(event)">
+                <div class="col-12 event" draggable @dragstart="drag">
+                  <p class="no-margin">{{ getEventHours(event) }}</p>
+                  <p v-if="event.type === INTERVENTION" class="no-margin">{{ event.customer.identity.title }} {{ event.customer.identity.lastname }}</p>
+                  <p v-if="event.type === ABSENCE">{{ displayAbsenceType(event.absence) }}</p>
+                  <p v-if="event.type === UNAVAILABILITY">Indisponibilité</p>
+                  <p v-if="event.type === INTERNAL_HOUR">{{ event.internalHour.name }}</p>
+                </div>
               </div>
-            </div>
-          </td>
-        </tr>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
 
@@ -404,6 +412,23 @@ export default {
         NotifyNegative('Erreur lors de la suppression du document');
       }
     },
+    // Drag & drop
+    drag (event) {
+      event.dataTransfer.setData('text', event.target.id);
+      event.target.style.color = 'green';
+      console.log('drag event =', event);
+    },
+    drop (event) {
+      console.log('drop event =', event);
+      // event.stopPropagation();
+      event.preventDefault();
+      const data = event.dataTransfer.getData('text');
+      console.log('data=', data);
+      event.target.appendChild(document.getElementById(data));
+    },
+    onDrop (res) {
+      console.log(res);
+    }
   }
 }
 </script>
@@ -411,7 +436,7 @@ export default {
 <style lang="stylus" scoped>
   table
     border-collapse: collapse
-    table-layout: fixed
+    // table-layout: fixed
   td
     border: 1px solid black
     padding: 5px
