f4cec5097064cdcd25211c1b450422f9031c2efa
Pierre HOFMAN <pierre.hofman@epitech.eu>
2019-01-22 15:11:50 +0100

    Feature 118 draggable events (#119)
    * Add library Draggable
    
    * Add draggable component, remove tests
    
    * Add lib 'vue-smooth-dnd' to test, add native HTML5 API d&d, add testing case from an img
    
    * Working drag&drop
    
    * Resolve drag and drop bugs if id is the same for two tds
    
    * Add debug.log in .gitignore
    
    * Take out testing libraries for drag & drop and remove some tests
    
    * Prevent target from dropping in same element
    
    * Remove unecessary code
    
    * Add library Draggable
    
    * Add lib 'vue-smooth-dnd' to test, add native HTML5 API d&d, add testing case from an img
    
    * Working drag&drop
    
    * Resolve drag and drop bugs if id is the same for two tds
    
    * Add debug.log in .gitignore
    
    * Take out testing libraries for drag & drop and remove some tests
    
    * Prevent target from dropping in same element
    
    * Remove unecessary code
    
    * Add library Draggable
    
    * Add lib 'vue-smooth-dnd' to test, add native HTML5 API d&d, add testing case from an img
    
    * Working drag&drop
    
    * Resolve drag and drop bugs if id is the same for two tds
    
    * Add debug.log in .gitignore
    
    * Take out testing libraries for drag & drop and remove some tests
    
    * Prevent target from dropping in same element
    
    * Add library Draggable
    
    * Remove unecessary code
    
    * Add lib 'vue-smooth-dnd' to test, add native HTML5 API d&d, add testing case from an img
    
    * Resolve drag and drop bugs if id is the same for two tds
    
    * Take out testing libraries for drag & drop and remove some tests
    
    * Remove unecessary code
    
    * Fix fail rebase strange horror things...

--- src/pages/proto/planning.vue
+++ src/pages/proto/planning.vue
@@ -9,28 +9,30 @@
         <q-btn icon="chevron_right" flat round @click="goToNextWeek"></q-btn>
       </div>
       <table style="width: 100%">
-        <tr>
-          <td></td>
-          <td class="capitalize" v-for="(day, index) in daysHeader" :key="index">
-            {{day}}
-          </td>
-        </tr>
-        <tr class="auxiliaries-row" v-for="(auxiliary, index) in auxiliaries" :key="index">
-          <td>
-            {{auxiliary.firstname}} {{auxiliary.lastname}}
-          </td>
-          <td v-for="(day, dayIndex) in days" :key="dayIndex" valign="top" class="event-cell">
-            <div class="row cursor-pointer" v-for="(event, eventIndex) in getAuxiliaryEvents(auxiliary, dayIndex)" :key="eventIndex" @click="openEditionModal(event)">
-              <div class="col-12 event">
-                <p class="no-margin">{{ getEventHours(event) }}</p>
-                <p v-if="event.type === INTERVENTION" class="no-margin">{{ event.customer.identity.title }} {{ event.customer.identity.lastname }}</p>
-                <p v-if="event.type === ABSENCE">{{ displayAbsenceType(event.absence) }}</p>
-                <p v-if="event.type === UNAVAILABILITY">Indisponibilité</p>
-                <p v-if="event.type === INTERNAL_HOUR">{{ event.internalHour.name }}</p>
+        <tbody>
+          <tr>
+            <td></td>
+            <td class="capitalize" v-for="(day, index) in daysHeader" :key="index">
+              {{day}}
+            </td>
+          </tr>
+          <tr class="auxiliaries-row" v-for="(auxiliary, index) in auxiliaries" :key="index">
+            <td>
+              {{auxiliary.firstname}} {{auxiliary.lastname}}
+            </td>
+            <td @drop="drop" @dragover.prevent v-for="(day, dayIndex) in days" :key="dayIndex" valign="top" class="event-cell">
+              <div :id="Math.random().toString(36).substr(2, 5)" draggable @dragstart="drag" class="row cursor-pointer" v-for="(event, eventIndex) in getAuxiliaryEvents(auxiliary, dayIndex)" :key="eventIndex" @click="openEditionModal(event)">
+                <div class="col-12 event">
+                  <p class="no-margin">{{ getEventHours(event) }}</p>
+                  <p v-if="event.type === INTERVENTION" class="no-margin">{{ event.customer.identity.title }} {{ event.customer.identity.lastname }}</p>
+                  <p v-if="event.type === ABSENCE">{{ displayAbsenceType(event.absence) }}</p>
+                  <p v-if="event.type === UNAVAILABILITY">Indisponibilité</p>
+                  <p v-if="event.type === INTERNAL_HOUR">{{ event.internalHour.name }}</p>
+                </div>
               </div>
-            </div>
-          </td>
-        </tr>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
 
@@ -404,6 +406,20 @@ export default {
         NotifyNegative('Erreur lors de la suppression du document');
       }
     },
+    // Drag & drop
+    drag (event) {
+      event.dataTransfer.setData('text', event.target.id);
+    },
+    drop (event) {
+      event.preventDefault();
+      const data = event.dataTransfer.getData('text');
+      if (event.target.nodeName === 'TD') {
+        event.target.appendChild(document.getElementById(data));
+      }
+      if (event.target.nodeName === 'P') {
+        event.target.parentNode.parentNode.parentNode.appendChild(document.getElementById(data));
+      }
+    }
   }
 }
 </script>
