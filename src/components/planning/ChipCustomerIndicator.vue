c69cc8ea01c33532f447074cc46604988809aa21
Sophie Moustard <sophie.moustard@gmail.com>
2019-02-21 16:15:12 +0100

    [180] Add customer indicator (#221)
    * [180] Add customer indicator
    
    * [180] Fix actions design
    
    * [180] Update ceil to round

---
+++ src/components/planning/ChipCustomerIndicator.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="full-width row relative-position">
+    <img :src="getAvatar(person.picture)" class="avatar chip-img">
+    <q-chip class="absolute-center" small text-color="white">
+      <span class="chip-indicator">{{ indicators.weeklyHours }}h - {{ indicators.auxiliariesNumber }}</span><q-icon size="14px" name="mdi-human-male" />
+    </q-chip>
+  </div>
+</template>
+
+<script>
+import { DEFAULT_AVATAR } from '../../data/constants.js';
+
+export default {
+  name: 'ChipCustomerIndicator',
+  props: {
+    events: { type: Array, default: () => [] },
+    person: { type: Object, default: () => ({}) },
+  },
+  computed: {
+    indicators () {
+      const auxiliaries = [];
+      let weeklyHours = 0;
+      this.events.forEach((event) => {
+        if (!auxiliaries.includes(event.auxiliary._id)) auxiliaries.push(event.auxiliary._id);
+        weeklyHours += this.$moment(event.endDate).diff(event.startDate, 'h', true);
+      });
+      return { auxiliariesNumber: auxiliaries.length, weeklyHours: Math.round(weeklyHours) };
+    },
+  },
+  methods: {
+    getAvatar (picture) {
+      return (!picture || !picture.link) ? DEFAULT_AVATAR : picture.link;
+    },
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~variables';
+
+  .chip-img
+    z-index: 10
+    &.avatar
+      box-shadow: none
+      border: 1px solid #979797
+      @media(min-width: 1025px)
+        width: 2.5rem
+        height: 2.5rem
+      @media(max-width: 1024px)
+        width: 1.5rem
+        height: 1.5rem
+
+  /deep/ .q-chip
+    width: 100%
+    background: $dark-grey
+    @media(max-width: 1024px)
+      font-size: 8px
+    @media(max-width: 768px)
+      display: none
+    &-main
+      justify-content: flex-end;
+      padding-left: 1.8rem;
+      overflow: visible;
+      display: flex;
+      align-items: center;
+      .q-icon
+        width: 8px;
+    &-small
+      @media(max-width: 1024px)
+        min-height: 20px
+
+  .chip-indicator
+    white-space: nowrap;
+
+</style>
