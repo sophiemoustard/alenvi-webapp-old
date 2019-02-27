60b3d1a1178157fc2cf7cfecf386001ec11940c1
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2019-02-27 15:25:49 +0100

    Cookies are now set to 1 day

--- src/pages/signin/Authenticate.vue
+++ src/pages/signin/Authenticate.vue
@@ -37,9 +37,6 @@
 </template>
 
 <script>
-
-import { date } from 'quasar'
-
 import CompaniHeader from '../../components/CompaniHeader';
 import { NotifyNegative } from '../../components/popup/notify';
 import { AUXILIARY, PLANNING_REFERENT } from '../../data/constants.js';
@@ -78,10 +75,10 @@ export default {
           email: this.credentials.email.toLowerCase(),
           password: this.credentials.password
         });
-        this.$q.cookies.set('alenvi_token', user.data.data.token, { path: '/', expires: date.addToDate(new Date(), { seconds: user.data.data.expiresIn }), secure: process.env.NODE_ENV !== 'development' });
-        this.$q.cookies.set('alenvi_token_expires_in', user.data.data.expiresIn, { path: '/', expires: date.addToDate(new Date(), { seconds: user.data.data.expiresIn }), secure: process.env.NODE_ENV !== 'development' });
+        this.$q.cookies.set('alenvi_token', user.data.data.token, { path: '/', expires: parseInt(user.data.data.expiresIn / 3600 / 24, 10), secure: process.env.NODE_ENV !== 'development' });
+        this.$q.cookies.set('alenvi_token_expires_in', user.data.data.expiresIn, { path: '/', expires: parseInt(user.data.data.expiresIn / 3600 / 24, 10), secure: process.env.NODE_ENV !== 'development' });
         this.$q.cookies.set('refresh_token', user.data.data.refreshToken, { path: '/', expires: 365, secure: process.env.NODE_ENV !== 'development' });
-        this.$q.cookies.set('user_id', user.data.data.user._id, { path: '/', expires: date.addToDate(new Date(), { seconds: user.data.data.expiresIn }), secure: process.env.NODE_ENV !== 'development' });
+        this.$q.cookies.set('user_id', user.data.data.user._id, { path: '/', expires: parseInt(user.data.data.expiresIn / 3600 / 24), secure: process.env.NODE_ENV !== 'development' });
         await this.$store.dispatch('main/getUser', this.$q.cookies.get('user_id'));
         if (this.$q.platform.is.desktop) {
           this.$store.commit('main/setToggleDrawer', true);
