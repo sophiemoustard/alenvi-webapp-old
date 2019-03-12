c8707b1574d2da877c03895227db63b7f7d53bfd
Sophie Moustard <sophie.moustard@gmail.com>
2019-03-12 11:39:38 +0100

    [211] Fix service creation (#274)

--- src/helpers/vuelidateCustomVal.js
+++ src/helpers/vuelidateCustomVal.js
@@ -3,30 +3,26 @@ const ibantools = require('ibantools');
 const axios = require('axios');
 
 export const frPhoneNumber = (value) => {
-  if (!value) {
-    return true;
-  }
+  if (!value) return true;
+
   return value.split(' ').join('').match(/[0]{1}[1-7]{1}[0-9]{8}/) || false;
 };
 
 export const frZipCode = (value) => {
-  if (!value) {
-    return false;
-  }
+  if (!value) return false;
+
   return value.split(' ').join('').match(/[0-9]{5}/) || false;
 }
 
 export const iban = (value) => {
-  if (!value) {
-    return false;
-  }
+  if (!value) return false;
+
   return ibantools.isValidIBAN(value.split(' ').join(''));
 }
 
 export const bic = (value) => {
-  if (!value) {
-    return false;
-  }
+  if (!value) return false;
+
   return ibantools.isValidBIC(value);
 }
 
@@ -53,4 +49,11 @@ export const sector = async (value) => {
   return new Promise(resolve => {
     resolve(res.length === 0);
   });
+}
+
+export const positiveNumber = (value) => {
+  if (!value) return true;
+  if (isNaN(parseFloat(value)) || !isFinite(value)) return false;
+
+  return value >= 0;
 }
