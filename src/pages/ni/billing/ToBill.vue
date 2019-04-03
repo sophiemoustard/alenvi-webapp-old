96253bf95b63f33aabcde4553c9969450ce794b4
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2019-04-03 12:01:26 +0200

    Rework of ToBill board (WIP)

--- src/pages/ni/billing/ToBill.vue
+++ src/pages/ni/billing/ToBill.vue
@@ -1,26 +1,43 @@
 <template>
-  <q-page class="neutral-background" padding>
+  <q-page class="neutral-background">
     <h4>À facturer</h4>
     <div class="q-mb-xl">
       <q-card style="background: white">
-        <q-table :data="draftBills" :columns="columns" row-key="name" hide-bottom binary-state-sort :pagination.sync="pagination">
+        <q-table :data="draftBills" :columns="columns" row-key="_id" hide-bottom binary-state-sort :pagination.sync="pagination" separator="none"
+          selection="multiple" :selected.sync="selected">
+
+          <q-tr slot="header" slot-scope="props">
+            <q-th auto-width>
+              <q-checkbox v-model="props.selected" indeterminate-value="some" />
+            </q-th>
+            <q-th v-for="col in props.cols" :key="col.name" :props="props">
+              {{ col.label }}
+            </q-th>
+          </q-tr>
+
           <template slot="body" slot-scope="props">
-            <q-tr :props="props">
+            <q-tr v-for="(bill, index) in props.row.bills" :key="bill._id" :props="props" :class="{'datatable-row-border-top': index === 0 }">
+              <q-td auto-width>
+                <q-checkbox v-if="props.row.bills.length === 1" v-model="props.selected" />
+              </q-td>
               <q-td v-for="col in props.cols" :key="col.name" :props="props">
-                <template>{{ col.value }}</template>
+                <template v-if="index === 0 && (col.name === 'startDate' || col.name === 'endDate')">{{ formatDate(bill[col.name]) }}</template>
+                <template v-else-if="col.name === 'service'">{{ bill.subscription.service.versions[bill.subscription.service.versions.length - 1].name }}</template>
+                <template v-else-if="col.name === 'hours'">{{ formatHours(bill.hours) }}</template>
+                <template v-else-if="col.name === 'unitPreTaxPrice'">{{ formatPrice(bill.unitPreTaxPrice) }}</template>
+                <template v-else-if="col.name === 'discount'">
+                  <ni-modal-input v-model="bill.discount" suffix="%" :margin-input="false" no-q-field />
+                </template>
+                <template v-else-if="col.name === 'preTaxPrice'">{{ formatPrice(bill.preTaxPrice) }}</template>
+                <template v-else-if="col.name === 'withTaxPrice'">{{ formatPrice(bill.withTaxPrice) }}</template>
+                <template v-else-if="index === 0">{{ col.value }}</template>
               </q-td>
             </q-tr>
-            <q-tr v-for="(bill, index) in props.row.bills.slice(1)" :key="index" :props="props">
-              <q-td v-for="col in props.cols" :key="col.name" :data-label="col.label" :props="props">
-                <template v-if="col.name === 'service'">{{ bill.subscription.service.versions[bill.subscription.service.versions.length - 1].name }}</template>
-                <template v-if="col.name === 'hours'">{{ formatHours(bill.hours) }}</template>
-                <template v-if="col.name === 'unitPreTaxPrice'">{{ formatPrice(bill.unitPreTaxPrice) }}</template>
-                <template v-if="col.name === 'discount'">{{ bill.discount }}</template>
-                <template v-if="col.name === 'preTaxPrice'">{{ formatPrice(bill.preTaxPrice) }}</template>
-                <template v-if="col.name === 'withTaxPrice'">{{ formatPrice(bill.withTaxPrice) }}</template>
-              </q-td>
-            </q-tr>
+
             <q-tr v-if="props.row.bills.length > 1" :props="props">
+              <q-td auto-width>
+                <q-checkbox v-model="props.selected" />
+              </q-td>
               <q-td v-for="col in props.cols" :key="col.name" :data-label="col.label" :props="props">
                 <template v-if="col.name === 'preTaxPrice'">Total :</template>
                 <template v-if="col.name === 'withTaxPrice'">{{ formatPrice(props.row.total) }}</template>
@@ -35,14 +52,19 @@
 
 <script>
 import { MONTH } from '../../../data/constants';
+import ModalInput from '../../../components/form/ModalInput';
 
 export default {
   name: 'ToBill',
-  components: {},
+  components: {
+    'ni-modal-input': ModalInput
+  },
   data () {
     return {
+      editDiscount: false,
       pagination: { rowsPerPage: 0 },
       draftBills: [],
+      selected: [],
       columns: [
         {
           name: 'status',
@@ -68,53 +90,49 @@ export default {
           name: 'startDate',
           label: 'Début F.',
           align: 'left',
-          field: row => row.bills[0].startDate ? this.$moment(row.startDate).format('DD/MM/YYYY') : '',
+          field: row => row.bills,
         },
         {
           name: 'endDate',
           label: 'Fin F.',
           align: 'left',
-          field: row => row.bills[0].endDate ? this.$moment(row.endDate).format('DD/MM/YYYY') : '',
+          field: row => row.bills,
         },
         {
           name: 'service',
           label: 'Service',
           align: 'left',
-          field: row => row.bills[0].subscription.service.versions[row.bills[0].subscription.service.versions.length - 1].name,
+          field: row => row.bills,
         },
         {
           name: 'hours',
           label: 'Décompte',
           align: 'center',
-          field: row => row.bills[0].hours,
-          format: value => this.formatHours(value),
+          field: row => row.bills,
         },
         {
           name: 'unitPreTaxPrice',
           label: 'PU HT',
           align: 'center',
-          field: row => row.bills[0].unitPreTaxPrice,
-          format: value => this.formatPrice(value),
+          field: row => row.bills,
         },
         {
           name: 'discount',
           label: 'Remise',
           align: 'center',
-          field: row => row.bills[0].discount,
+          field: row => row.bills,
         },
         {
           name: 'preTaxPrice',
           label: 'HT',
           align: 'center',
-          field: row => row.bills[0].preTaxPrice,
-          format: value => this.formatPrice(value),
+          field: row => row.bills,
         },
         {
           name: 'withTaxPrice',
           label: 'TTC',
           align: 'center',
-          field: row => row.bills[0].withTaxPrice,
-          format: value => this.formatPrice(value),
+          field: row => row.bills,
         },
       ],
     }
@@ -147,6 +165,7 @@ export default {
         startDate: this.billingPeriod.startDate.toDate(),
         billingPeriod: this.user.company.customersConfig.billingPeriod,
       });
+      this.draftBills = this.draftBills.map(bill => ({ ...bill, editDiscount: false }));
     } catch (e) {
       this.draftBills = [];
       console.error(e);
@@ -159,13 +178,21 @@ export default {
     formatHours (value) {
       return value ? `${parseFloat(value).toFixed(2)}h` : '';
     },
+    formatDate (value) {
+      return value ? `${this.$moment(value).format('DD/MM/YY')}` : '';
+    }
   },
 }
 </script>
 
 <style lang="stylus" scoped>
+  @import '~variables'
 
   .layout-padding
     padding: 2rem 3rem;
+
+  .editable
+    color: $primary
+    cursor: pointer
 
 </style>
