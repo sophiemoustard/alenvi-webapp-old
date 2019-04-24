95e3b42f493c053f5620b3d797309ad7d2a01fc2
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2019-04-24 13:51:39 +0200

    Add handling of payments batch creation

--- src/api/Payments.js
+++ src/api/Payments.js
@@ -1,4 +1,6 @@
+import moment from 'moment'
 import { alenviAxios } from './ressources/alenviAxios'
+import { downloadFile } from '../helpers/downloadFile'
 
 export default {
   async list (params) {
@@ -6,6 +8,15 @@ export default {
     return payments.data.data.payments;
   },
   async create (data) {
+    if (Array.isArray(data)) {
+      const file = await alenviAxios({
+        url: `${process.env.API_HOSTNAME}/payments`,
+        method: 'POST',
+        responseType: 'blob',
+        data,
+      });
+      return downloadFile(file, `prélèvements_${moment().format('YYYYMMDD_HHmm')}.xml`);
+    }
     const payment = await alenviAxios.post(`${process.env.API_HOSTNAME}/payments`, data);
     return payment.data.data.payment;
   },
