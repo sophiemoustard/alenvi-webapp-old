f52b6c9d159304db8f7f0c09701a56427d922d57
Sophie Moustard <sophie.moustard@gmail.com>
2019-04-29 16:45:56 +0200

    Add ttc ro discount title

--- src/pages/ni/billing/ToBill.vue
+++ src/pages/ni/billing/ToBill.vue
@@ -112,7 +112,7 @@ export default {
         },
         {
           name: 'discount',
-          label: 'Remise',
+          label: 'Remise TTC',
           align: 'center',
         },
         {
