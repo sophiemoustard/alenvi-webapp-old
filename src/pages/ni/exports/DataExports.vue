19dca8421dd2f4f43808eed4cb48de373b098076
Sophie Moustard <sophie.moustard@gmail.com>
2019-05-16 10:46:48 +0200

    [284] Add pages

---
+++ src/pages/ni/exports/DataExports.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="neutral-background q-pb-xl">
+    <div class="title-padding">
+      <h4>Données</h4>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'DataExports',
+  metaInfo: { title: 'Données' },
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
