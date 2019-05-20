defb03e18061ab964a14ad974ae2f20aa359ad8c
Sophie Moustard <sophie.moustard@gmail.com>
2019-05-20 14:43:57 +0200

    [276] Add contract hours, worked hours and dates

--- src/pages/ni/pay/ToPay.vue
+++ src/pages/ni/pay/ToPay.vue
@@ -3,6 +3,7 @@
     <div class="title-padding">
       <h4>À payer</h4>
     </div>
+    <q-table :data="darftPay" :columns="columns" class="q-pa-sm" />
   </q-page>
 </template>
 
@@ -13,6 +14,48 @@ export default {
   data () {
     return {
       darftPay: [],
+      columns: [
+        {
+          name: 'auxiliary',
+          label: 'Auxiliaire',
+          align: 'left',
+          field: 'auxiliary',
+          format: value => value && value.identity ? `${value.identity.firstname.slice(0, 1)}. ${value.identity.lastname}` : ''
+        },
+        {
+          name: 'sector',
+          label: 'Secteur',
+          align: 'left',
+          field: 'auxiliary',
+          format: value => value && value.sector ? value.sector.name : '',
+        },
+        {
+          name: 'startDate',
+          label: 'Début de paye',
+          align: 'left',
+          field: 'startDate',
+          format: value => value ? this.$moment(value).format('DD/MM/YYYY') : '',
+        },
+        {
+          name: 'endDate',
+          label: 'Fin de paye',
+          align: 'left',
+          field: 'endDate',
+          format: value => value ? this.$moment(value).format('DD/MM/YYYY') : '',
+        },
+        {
+          name: 'contractHours',
+          label: 'Heures contrat',
+          align: 'center',
+          field: 'contractHours',
+        },
+        {
+          name: 'workedHours',
+          label: 'Total heures travaillées',
+          align: 'center',
+          field: 'workedHours',
+        },
+      ],
     };
   },
   async mounted () {
@@ -22,8 +65,8 @@ export default {
     async refreshDraftPay () {
       try {
         this.darftPay = await this.$pay.getDraftPay({
-          startDate: this.$moment().startOf('m').startOf('d').toISOString(),
-          endDate: this.$moment().endOf('m').endOf('d').toISOString(),
+          startDate: this.$moment().startOf('M').startOf('d').toISOString(),
+          endDate: this.$moment().endOf('M').endOf('d').toISOString(),
         });
       } catch (e) {
         this.darftPay = [];
