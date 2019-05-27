e54ae6e04a21fae8aa046738d664d7b410bff6e8
Camille Alemany <camillealemany23@gmail.com>
2019-05-27 14:39:49 +0200

    [289] Add HistoryExports page skeleton

--- src/pages/ni/exports/HistoryExports.vue
+++ src/pages/ni/exports/HistoryExports.vue
@@ -1,17 +1,63 @@
 <template>
-  <q-page class="neutral-background q-pb-xl">
-    <div class="title-padding">
-      <h4>Historique</h4>
+  <q-page padding class="neutral-background q-pb-xl">
+    <h4>Historique</h4>
+    <div class="row gutter-sm">
+      <ni-select caption="Type d'export" :options="exportTypeOptions" v-model="type" />
+      <ni-date-range class="col-12 col-md-auto" caption="Période" v-model="dateRange" />
     </div>
+    <q-btn label="Exporter" no-caps flat color="white" icon="import_export" @click="exportCsv" />
   </q-page>
 </template>
 
 <script>
+import _ from 'lodash';
+
+import { NotifyNegative, NotifyPositive } from '../../../components/popup/notify';
+import { downloadFile } from '../../../helpers/downloadFile';
+import Select from '../../../components/form/Select';
+import DateRange from '../../../components/form/DateRange';
+import { EXPORT_HISTORY_TYPES } from '../../../data/constants';
+
 export default {
   name: 'History',
   metaInfo: { title: 'Historique' },
+  components: {
+    'ni-select': Select,
+    'ni-date-range': DateRange
+  },
+  data () {
+    return {
+      exportTypeOptions: EXPORT_HISTORY_TYPES,
+      type: null,
+      dateRange: { startDate: this.$moment().subtract(1, 'year').toISOString(), endDate: this.$moment().toISOString() }
+    }
+  },
+  methods: {
+    async exportCsv () {
+      try {
+        const type = _.find(EXPORT_HISTORY_TYPES, ['value', this.type]);
+        if (!type) {
+          NotifyNegative('Impossible de téléchager le document');
+          return;
+        }
+
+        const csv = await this.$exports.getHistoryCsv({ ...this.dateRange, type: type.value });
+        await downloadFile(csv, `${type.label}.csv`);
+
+        NotifyPositive('Document téléchargé');
+      } catch (e) {
+        console.error(e);
+        NotifyNegative('Erreur lors du téléchagement du document');
+      }
+    },
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
+@import '~variables';
+
+  .q-btn
+    background-color: $primary;
+
 </style>
