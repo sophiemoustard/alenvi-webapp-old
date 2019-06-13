9f9ed1743ccf497da0d3d0baaf47d08402f6fb58
Sophie Moustard <sophie.moustard@gmail.com>
2019-06-13 11:23:24 +0200

    [313] Remove SelectOgustList

--- src/api/Ogust.js
+++ src/api/Ogust.js
@@ -7,15 +7,6 @@ export default {
     const ogustToken = res.data.data.token;
     return ogustToken;
   },
-  async getList (key, ogustToken = null) {
-    let ogustListRaw = {};
-    if (ogustToken == null) {
-      ogustListRaw = await alenviAxios.post(`${process.env.API_HOSTNAME}/ogust/utils/getList?key=${key}`, {});
-    } else {
-      ogustListRaw = await axios.post(`${process.env.API_HOSTNAME}/ogust/utils/getList?key=${key}`, {}, { headers: { 'x-ogust-token': ogustToken } });
-    }
-    return ogustListRaw.data.data.list;
-  },
   async getEmployeeSalaries (employeeId) {
     const salaries = await alenviAxios.get(`${process.env.API_HOSTNAME}/ogust/employees/${employeeId}/salaries`);
     return salaries.data.data.salaries.array_salary.result;
