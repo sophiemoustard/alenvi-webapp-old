eda209058a1f213df61614fed6d73bb1ab31d44f
Sophie Moustard <sophie.moustard@gmail.com>
2019-07-19 16:09:22 +0200

    COM-524 - Fix drag and drop on customer planning (#483)

--- src/pages/ni/planning/CustomerPlanning.vue
+++ src/pages/ni/planning/CustomerPlanning.vue
@@ -464,8 +464,8 @@ export default {
           .minutes(this.$moment(draggedObject.startDate).minutes()).toISOString(),
         endDate: this.$moment(toDay).add(daysBetween, 'days').hours(this.$moment(draggedObject.endDate).hours())
           .minutes(this.$moment(draggedObject.endDate).minutes()).toISOString(),
-        auxiliary: draggedObject.auxiliary._id,
       };
+      if (draggedObject.auxiliary) payload.auxiliary = draggedObject.auxiliary._id;
 
       const hasConflicts = await this.hasConflicts(payload);
       if (hasConflicts) {
