88277cc94c470e3c51705d250a1b5971889b0cf2
jctrebalag <jctrebalag@gmail.com>
2019-08-26 12:03:40 +0200

    COM-543 Replace content of salaries page by ProfilePay component

--- src/pages/auxiliaries/administrative/Salaries.vue
+++ src/pages/auxiliaries/administrative/Salaries.vue
@@ -1,121 +1,28 @@
 <template>
   <q-page padding class="neutral-background">
-    <h4>Fiches de paie</h4>
-    <q-table
-      :data="salaries"
-      :columns="columns"
-      row-key="name"
-      :rows-per-page-options="[10]"
-      :pagination.sync="pagination"
-      :loading="tableLoading">
-        <q-td slot="body-cell-periodStart" slot-scope="props" :props="props">
-          {{ props.value }}
-        </q-td>
-        <q-td slot="body-cell-link" slot-scope="props" :props="props">
-          <q-btn flat round small color="primary">
-            <a :href="props.value">
-              <q-icon name="file download" />
-            </a>
-          </q-btn>
-        </q-td>
-    </q-table>
+    <div v-if="currentUser">
+      <h4>Documents de paie</h4>
+      <ni-profile-pay />
+    </div>
   </q-page>
 </template>
 
 <script>
+
+import ProfilePay from '../../../components/auxiliary/ProfilePay';
+
 export default {
-  metaInfo: {
-    title: 'Fiches de paie',
+  props: ['id'],
+  components: {
+    'ni-profile-pay': ProfilePay,
   },
-  data () {
-    return {
-      tableLoading: false,
-      salaries: [],
-      pagination: {
-        sortBy: 'periodStart',
-        descending: true,
-        page: 1,
-        rowsPerPage: 10,
-      },
-      columns: [
-        {
-          name: 'periodStart',
-          label: 'Date',
-          field: 'period_start',
-          align: 'left',
-          sortable: true,
-          format: (value) => this.$moment(value, 'YYYYMMDDHHmm').format('MMMM YYYY'),
-          sort: (a, b) => (this.$moment(a, 'YYYYMMDDHHmm').toDate()) - (this.$moment(b, 'YYYYMMDDHHmm').toDate()),
-          style: 'width: 170px',
-        },
-        {
-          name: 'link',
-          label: 'Visualiser',
-          field: 'print_url',
-          align: 'left',
-          style: 'width: 30px',
-        },
-      ],
-    }
+  metaInfo: {
+    title: 'Documents de paie',
   },
   computed: {
-    user () {
+    currentUser () {
       return this.$store.getters['main/user'];
     },
   },
-  async mounted () {
-    try {
-      if (!this.user.employee_id) return;
-
-      this.tableLoading = true;
-      const salaries = await this.$ogust.getEmployeeSalaries(this.user.employee_id);
-      for (const k in salaries) {
-        this.salaries.push(salaries[k]);
-      }
-      this.tableLoading = false;
-    } catch (e) {
-      console.error(e);
-      this.tableLoading = false;
-    }
-  },
 }
 </script>
-
-<style lang="stylus" scoped>
-@import '~variables'
-
-/deep/ .q-table
-  border-spacing: 0 12px
-  border-collapse: separate
-  &-horizontal-separator tbody td
-    border: none
-  & thead
-    border: none
-    & tr
-      height: 48px
-  & th
-    padding: 0px 12px
-    &.sortable:hover .q-icon, &.sorted .q-icon
-      color: $primary
-  & tbody tr
-    background: $white
-  & td
-    padding: 8px 12px
-    font-size: 16px
-    & .q-item
-      min-height: 30px
-      padding: 0
-      &-main
-        flex: 0 1 auto
-      & .q-item-side
-        min-width: 30px
-        max-height: 30px
-      & .q-item-section + .q-item-section
-        margin-left: 20px
-  &-container
-    & .q-table-bottom
-      padding-left: 12px
-    & .q-table-nodata > i
-      display: none
-
-</style>
