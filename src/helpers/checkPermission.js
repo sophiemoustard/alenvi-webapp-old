12822a39329d49e95ca9c5042142580d76f1b934
jctrebalag <jctrebalag@gmail.com>
2019-08-27 14:35:48 +0200

    COM-595 Rework of route/app permission checking

--- src/helpers/checkPermission.js
+++ src/helpers/checkPermission.js
@@ -1,29 +1,19 @@
-import { Cookies } from 'quasar';
-
+import intersection from 'lodash/intersection';
 import redirect from '../router/redirect';
 
-const findPermission = (right, paramsId) => {
-  return (permission) => {
-    if (typeof permission === 'string') {
-      return permission === right.permission && right.hasAccess;
-    } else if (paramsId && typeof permission.name === 'string' && typeof permission.when === 'function') {
-      return permission.name === right.permission && right.hasAccess && permission.when(paramsId, Cookies.get('user_id'));
-    }
-  };
-};
+export const checkPermission = (to, user) => {
+  if (to.meta.permissions) {
+    if (user.role && user.role.rights) {
+      const rights = user.role.rights.map(right => right.permission);
 
-export const checkPermission = async (to, user) => {
-  try {
-    if (to.meta.permissions) {
-      if (user.role && user.role.rights) {
-        return user.role.rights.filter(right => right ? to.meta.permissions.find(findPermission(right, to.params.id || null)) : false).length > 0;
-      } else {
-        return redirect.redirectToLogin({ to });
-      }
+      const count = typeof to.meta.permissions === 'string'
+        ? rights.includes(to.meta.permissions)
+        : intersection(rights, to.meta.permissions).length;
+
+      return !!count;
+    } else {
+      return redirect.redirectToLogin({ to });
     }
-    return true;
-  } catch (e) {
-    console.error(e);
-    return false;
   }
-};
+  return true;
+}
