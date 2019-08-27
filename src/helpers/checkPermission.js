f3295b00c00dc8ec381585e4d61e5cd7f9f880dc
jctrebalag <jctrebalag@gmail.com>
2019-08-27 15:33:47 +0200

    COM-595 Retour Sophie

--- src/helpers/checkPermission.js
+++ src/helpers/checkPermission.js
@@ -2,18 +2,24 @@ import intersection from 'lodash/intersection';
 import redirect from '../router/redirect';
 
 export const checkPermission = (to, user) => {
-  if (to.meta.permissions) {
-    if (user.role && user.role.rights) {
-      const rights = user.role.rights.map(right => right.permission);
+  try {
+    if (to.meta.permissions) {
+      if (user.role && user.role.rights) {
+        const rights = user.role.rights.map(right => right.permission);
 
-      const count = typeof to.meta.permissions === 'string'
-        ? rights.includes(to.meta.permissions)
-        : intersection(rights, to.meta.permissions).length;
+        const count =
+          typeof to.meta.permissions === 'string'
+            ? rights.includes(to.meta.permissions)
+            : intersection(rights, to.meta.permissions).length;
 
-      return !!count;
-    } else {
-      return redirect.redirectToLogin({ to });
+        return !!count;
+      } else {
+        return redirect.redirectToLogin({ to });
+      }
     }
+    return true;
+  } catch (e) {
+    console.error(e);
+    return false;
   }
-  return true;
-}
+};
