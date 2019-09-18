5286c0f78f418deeea70ba7d0f7118193690871c
Camille Alemany <camillealemany23@gmail.com>
2019-09-18 11:30:19 +0200

    COM-596 Add customer's helpers on follow up page (#567)

---
+++ src/components/users/HelperList.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-table :data="sortedHelpers" :columns="columns" row-key="name" :pagination="pagination" hide-bottom>
+  </q-table>
+</template>
+
+<script>
+import { formatIdentity } from '../../helpers/utils';
+
+export default {
+  name: 'HelperList',
+  props: {
+    helpers: { type: Array, default: () => [] },
+  },
+  data () {
+    return {
+      columns: [
+        {
+          name: 'identity',
+          label: 'Identité',
+          align: 'left',
+          field: row => formatIdentity(row.identity, 'FL'),
+        },
+        {
+          name: 'email',
+          label: 'Email',
+          align: 'left',
+          field: row => row.local.email,
+        },
+      ],
+      pagination: { rowsPerPage: 0 },
+    };
+  },
+  computed: {
+    sortedHelpers () {
+      return [...this.helpers].sort((u1, u2) => {
+        return (u1.identity.lastname || '').localeCompare((u2.identity.lastname || ''));
+      });
+    },
+  },
+}
+</script>
