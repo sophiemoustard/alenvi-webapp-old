413285bd5305ea1493ed385fc4ff3460dafe48dd
Sophie Moustard <sophie.moustard@gmail.com>
2019-09-19 20:21:51 +0200

    COM-591 - use profileFollowUp component on auxiliary account

--- src/pages/auxiliaries/customers/CustomerInfo.vue
+++ src/pages/auxiliaries/customers/CustomerInfo.vue
@@ -1,68 +1,46 @@
 <template>
   <q-page padding class="neutral-background">
-    <div v-if="isLoaded">
+    <div v-if="userProfile">
       <div class="row items-center col-xs-12 header-margin">
         <div>
           <q-icon class="on-left cursor-pointer self-center" size="1rem" name="arrow_back" color="primary"
             @click.native="$router.go(-1)" />
         </div>
-        <h4 class="no-margin">{{ customer.identity | formatIdentity('FL') }}</h4>
+        <h4 class="no-margin">{{ userProfile.identity | formatIdentity('FL') }}</h4>
       </div>
-      <div class="row">
-        <p class="text-weight-bold">Informations</p>
-      </div>
-      <div class="row gutter-profile q-mb-lg">
-        <ni-input caption="Adresse" v-model="customer.contact.address.fullAddress" disable />
-        <ni-input caption="Accès / codes" v-model="customer.contact.accessCodes" @focus="saveTmp('contact.accessCodes')"
-          @blur="updateCustomer('contact.accessCodes')" />
-      </div>
-      <div class="row gutter-profile q-mb-lg">
-        <ni-input  caption="Environnement du bénéficiaire" v-model="customer.followUp.environment" :rows="6" type="textarea"
-          @blur="updateCustomer('followUp.environment')" @focus="saveTmp('followUp.environment')" />
-        <ni-input  caption="Objectifs de l’accompagnement" v-model="customer.followUp.objectives" :rows="6" type="textarea"
-          @blur="updateCustomer('followUp.objectives')" @focus="saveTmp('followUp.objectives')" />
-        <ni-input  caption="Autres" v-model="customer.followUp.misc" :rows="1" type="textarea"
-          @blur="updateCustomer('followUp.misc')" @focus="saveTmp('followUp.misc')" />
-      </div>
+      <profile-follow-up />
     </div>
   </q-page>
 </template>
 
 <script>
-import { followUpMixin } from '../../../mixins/followUpMixin.js';
-import Input from '../../../components/form/Input';
-import Select from '../../../components/form/Select';
 import { formatIdentity } from '../../../helpers/utils';
+import ProfileFollowUp from '../../../components/customers/ProfileFollowUp.vue';
 
 export default {
   components: {
-    'ni-input': Input,
-    'ni-select': Select,
+    'profile-follow-up': ProfileFollowUp,
   },
   props: {
     id: { type: String },
     customerId: { type: String },
   },
-  mixins: [followUpMixin],
   metaInfo: {
     title: 'Fiche bénéficiaire',
   },
-  data () {
-    return {
-      tmpInput: '',
-      isLoaded: false,
-      customer: {
-        identity: {},
-        contact: { address: {} },
-        followUp: {},
-      },
-    }
+  computed: {
+    userProfile () {
+      return this.$store.getters['rh/getUserProfile'];
+    },
   },
   async mounted () {
-    await this.getCustomer(this.customerId);
+    await this.$store.dispatch('rh/getUserProfile', { customerId: this.customerId });
   },
   filters: {
     formatIdentity,
+  },
+  beforeDestroy () {
+    this.$store.commit('rh/saveUserProfile', null);
   },
 }
 </script>
