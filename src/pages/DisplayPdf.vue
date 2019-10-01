c35af3aa629706e3313022403a2a04f2178767a5
jctrebalag <jctrebalag@gmail.com>
2019-10-01 17:14:14 +0200

    COM-562 Add message if pdf cannot display

--- src/pages/DisplayPdf.vue
+++ src/pages/DisplayPdf.vue
@@ -1,6 +1,8 @@
 <template>
   <div style="width: 100vw; height: 100vh">
     <object :data="blobUrl" :name="fileName" type="application/pdf" width="100%" height="100%" />
+    <big>Votre navigateur ne peut pas charger l'aperçu, vous pouvez télécharger votre document en cliquant
+       <a :href="blobUrl" :download="fileName">ici</a></big>
   </div>
 </template>
 
