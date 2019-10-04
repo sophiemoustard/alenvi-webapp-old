225de2214509b878139639683327954eda67ecb7
Chloe Molle <chloemolle@MacBook-Pro-de-Chloe.local>
2019-10-04 17:18:54 +0200

    COM-585 first version version edition modal

---
+++ src/components/contracts/VersionEditionModal.vue
@@ -0,0 +1,48 @@
+<template>
+  <ni-modal :value="isOpened">
+    <template slot="title">
+      Modifier le <span class="text-weight-bold">contrat</span>
+    </template>
+    <ni-input in-modal caption="Taux horaire"  type="number" suffix="€" required-field
+      v-model="editedVersion.grossHourlyRate" :error="validations.grossHourlyRate.$error"
+      @blur="validations.grossHourlyRate.$touch" />
+    <ni-datetime-picker caption="Date d'effet" v-model="editedVersion.startDate" :min="minStartDate"
+      in-modal required-field @blur="validations.startDate.$touch" :error="validations.startDate.$error" />
+    <div class="margin-input last">
+      <q-checkbox v-model="editedVersion.shouldBeSigned" label="Signature en ligne" />
+    </div>
+    <template slot="footer">
+      <q-btn no-caps class="full-width modal-btn" label="Modifier le contrat" icon-right="add" color="primary"
+        :loading="loading" @click="editVersion" :disable="!isVersionUpdated" />
+    </template>
+  </ni-modal>
+</template>
+
+<script>
+import Input from '../form/Input';
+import DatetimePicker from '../form/DatetimePicker';
+import Modal from '../Modal';
+
+export default {
+  name: 'VersionEditionModal',
+  props: {
+    isOpened: { type: Boolean, default: false },
+    editedVersion: { type: Object, default: () => ({}) },
+    validations: { type: Object, default: () => ({}) },
+    minStartDate: { type: String, default: '' },
+    versionTemplate: { type: String, default: '' },
+    isVersionUpdated: { type: Boolean, default: false },
+    loading: {type: Boolean, default: false},
+  },
+  components: {
+    'ni-input': Input,
+    'ni-datetime-picker': DatetimePicker,
+    'ni-modal': Modal,
+  },
+  methods: {
+    editVersion () {
+      this.$emit('editVersion');
+    },
+  },
+}
+</script>
--- src/mixins/contractMixin.js
+++ src/mixins/contractMixin.js
@@ -1,4 +1,10 @@
 import nationalities from '../data/nationalities.js';
+import { NotifyPositive, NotifyNegative, NotifyWarning } from '../components/popup/notify';
+import { required, minValue } from 'vuelidate/lib/validators';
+import { minDate } from '../helpers/vuelidateCustomVal';
+import { COMPANY_CONTRACT, CUSTOMER_CONTRACT } from '../data/constants';
+import { generateContractFields } from '../helpers/generateContractFields.js';
+import { translate } from '../data/translate';
 
 export const contractMixin = {
   data () {
@@ -6,7 +12,102 @@ export const contractMixin = {
       timeout: null,
     }
   },
+  validations () {
+    return {
+      newVersion: {
+        weeklyHours: { required },
+        startDate: { required },
+        grossHourlyRate: { required },
+      },
+      editedVersion: {
+        grossHourlyRate: { required, minValue: minValue(0) },
+        startDate: { required, minDate: this.editedVersionMinStartDate ? minDate(this.editedVersionMinStartDate) : '' },
+      },
+    }
+  },
   methods: {
+    isVersionUpdated () {
+      if (this.selectedVersion.grossHourlyRate !== this.editedVersion.grossHourlyRate) return true;
+      if (!this.$moment(this.selectedVersion.startDate).isSame(this.editedVersion.startDate)) return true;
+
+      return !!this.$_.get(this.selectedVersion, 'signature.eversignId') !== this.editedVersion.shouldBeSigned;
+    },
+    async getSignaturePayload (contract, title, template) {
+      const signature = {
+        ...this.esignRedirection,
+        templateId: template.driveId,
+        meta: { status: contract.status, auxiliaryDriveId: this.getUser.administrative.driveFolder.driveId },
+        fields: generateContractFields(
+          contract.status,
+          { user: this.getUser, contract: contract, initialContractStartDate: this.selectedContract.startDate }
+        ),
+      }
+
+      if (contract.status === CUSTOMER_CONTRACT) {
+        const helpers = await this.$users.showAll({ customers: contract.customer });
+        const currentCustomer = helpers[0].customers.find(cus => cus._id === contract.customer);
+        signature.signers = this.generateContractSigners({ name: helpers[0].identity.lastname, email: helpers[0].local.email });
+        signature.title = `${translate[contract.status]} - ${currentCustomer.identity.lastname}`;
+        signature.meta.customerDriveId = currentCustomer.driveFolder.driveId;
+      } else {
+        signature.signers = this.generateContractSigners(
+          { name: `${this.mainUser.identity.firstname} ${this.mainUser.identity.lastname}`, email: this.mainUser.local.email }
+        );
+        signature.title = `${title}${translate[contract.status]} - ${this.userFullName}`;
+      }
+
+      return signature;
+    },
+    getVersionTemplate (contract) {
+      return contract.status === COMPANY_CONTRACT
+        ? this.$_.get(this.userCompany, 'rhConfig.templates.contractWithCompanyVersion')
+        : this.$_.get(this.userCompany, 'rhConfig.templates.contractWithCustomerVersion');
+    },
+    async getVersionEditionPayload () {
+      const payload = this.$_.pick(this.editedVersion, ['startDate', 'grossHourlyRate']);
+      if (this.editedVersion.shouldBeSigned) {
+        const versionMix = { ...this.selectedContract, ...this.editedVersion };
+        const template = this.getVersionTemplate(versionMix);
+        payload.signature = await this.getSignaturePayload(versionMix, 'Avenant au ', template);
+      }
+
+      return this.$_.pickBy(payload);
+    },
+    async editVersion () {
+      try {
+        if (!this.isVersionUpdated) return this.resetVersionEditionModal();
+        if (this.isPreviousPayImpacted) {
+          await this.$q.dialog({
+            title: 'Confirmation',
+            message: 'Ce changement impacte une paie déjà effectuée. Vérifiez que vous ne pouvez pas créer un avenant prenant effet ce mois-ci. Confirmez-vous ce changement ?',
+            ok: true,
+            cancel: 'Annuler',
+          });
+        }
+
+        this.$v.editedVersion.$touch();
+        if (this.$v.editedVersion.$error) return NotifyWarning('Champ(s) invalide(s)');
+
+        this.loading = true;
+        const payload = await this.getVersionEditionPayload();
+        const params = { contractId: this.editedVersion.contractId, versionId: this.editedVersion.versionId }
+        await this.$contracts.updateVersion(params, payload);
+        await this.refreshContracts();
+
+        this.resetVersionEditionModal();
+        NotifyPositive('Contrat modifié');
+      } catch (e) {
+        console.error(e);
+        if (e.message === '') return NotifyPositive('Edition annulée');
+        if (e.data && e.data.statusCode === 422) {
+          this.$v.editedVersion.$reset();
+          return NotifyNegative('La date de début du contrat doit etre antérieure aux évènements de l\'auxiliaire.');
+        }
+        NotifyNegative('Erreur lors de l\'edition du contrat');
+      } finally {
+        this.loading = false;
+      }
+    },
     getFullNationality (nationality) {
       return nationalities[nationality];
     },
